2adf3e5a2ab2325b359b7c6a9fe0817c957f942e
minorg <minorg@server.fake>
2015-11-08 18:36:06 -0600

    delete InstitutionByIdView until/unless it is needed

--- java/gui/src/main/java/net/lab1318/costume/gui/presenters/institution/InstitutionsPresenter.java
+++ java/gui/src/main/java/net/lab1318/costume/gui/presenters/institution/InstitutionsPresenter.java
@@ -1,48 +1,70 @@
 package net.lab1318.costume.gui.presenters.institution;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
 import org.notaweb.gui.EventBus;
 import org.notaweb.gui.presenters.Presenter;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.common.eventbus.Subscribe;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.google.inject.servlet.SessionScoped;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.SystemError;
 import com.vaadin.ui.UI;
 
+import net.lab1318.costume.api.models.collection.CollectionEntry;
 import net.lab1318.costume.api.models.institution.InstitutionEntry;
 import net.lab1318.costume.api.services.IoException;
+import net.lab1318.costume.api.services.collection.CollectionQueryService;
+import net.lab1318.costume.api.services.collection.CollectionQueryService.Messages.GetCollectionByIdRequest;
 import net.lab1318.costume.api.services.institution.InstitutionQueryService;
-import net.lab1318.costume.api.services.institution.InstitutionQueryService.Messages.GetInstitutionByIdRequest;
-import net.lab1318.costume.gui.views.institution.InstitutionByIdView;
+import net.lab1318.costume.gui.views.collection.CollectionByIdView;
 import net.lab1318.costume.gui.views.institution.InstitutionsView;
 
 @SessionScoped
 public class InstitutionsPresenter extends Presenter<InstitutionsView> {
     @Inject
-    public InstitutionsPresenter(final EventBus eventBus, final Injector injector, final InstitutionsView view) {
+    public InstitutionsPresenter(final CollectionQueryService collectionQueryService, final EventBus eventBus,
+            final InstitutionQueryService institutionQueryService, final InstitutionsView view) {
         super(eventBus, view);
-        institutionQueryService = injector.getInstance(InstitutionQueryService.class);
+        this.collectionQueryService = checkNotNull(collectionQueryService);
+        this.institutionQueryService = checkNotNull(institutionQueryService);
     }
 
     @Subscribe
-    public void onGetInstitutionByIdRequest(final GetInstitutionByIdRequest request) {
-        UI.getCurrent().getNavigator().navigateTo(InstitutionByIdView.NAME + "/" + request.getId().toString());
+    public void onGetCollectionByIdRequest(final GetCollectionByIdRequest request) {
+        UI.getCurrent().getNavigator().navigateTo(CollectionByIdView.NAME + "/" + request.getId().toString());
     }
 
     @Override
     protected void _onViewEnter(final ViewChangeEvent event) {
-        final ImmutableList<InstitutionEntry> institutions;
+        final ImmutableList<CollectionEntry> collectionEntries;
+        final ImmutableList<InstitutionEntry> institutionEntries;
         try {
-            institutions = institutionQueryService.getInstitutions();
+            collectionEntries = collectionQueryService.getCollections();
+            institutionEntries = institutionQueryService.getInstitutions();
         } catch (final IoException e) {
             _getView().setComponentError(new SystemError("I/O exception", e));
             return;
         }
-        _getView().setModels(institutions);
+
+        final ImmutableMultimap.Builder<InstitutionEntry, CollectionEntry> institutionTreeBuilder = ImmutableMultimap
+                .builder();
+
+        for (final CollectionEntry collectionEntry : collectionEntries) {
+            for (final InstitutionEntry institutionEntry : institutionEntries) {
+                if (institutionEntry.getId().equals(collectionEntry.getModel().getInstitutionId())) {
+                    institutionTreeBuilder.put(institutionEntry, collectionEntry);
+                    break;
+                }
+            }
+        }
+
+        _getView().setModels(institutionTreeBuilder.build());
     }
 
+    private final CollectionQueryService collectionQueryService;
     private final InstitutionQueryService institutionQueryService;
 }
--- java/gui/src/main/java/net/lab1318/costume/gui/views/institution/InstitutionsView.java
+++ java/gui/src/main/java/net/lab1318/costume/gui/views/institution/InstitutionsView.java
@@ -1,16 +1,23 @@
 package net.lab1318.costume.gui.views.institution;
 
+import java.util.Collection;
+import java.util.Map;
+
 import org.notaweb.gui.EventBus;
 
-import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMultimap;
 import com.google.inject.Inject;
 import com.google.inject.servlet.SessionScoped;
-import com.vaadin.ui.Button;
-import com.vaadin.ui.Button.ClickEvent;
-import com.vaadin.ui.GridLayout;
+import com.vaadin.data.Item;
+import com.vaadin.event.ItemClickEvent;
+import com.vaadin.event.ItemClickEvent.ItemClickListener;
+import com.vaadin.ui.AbstractSelect.ItemCaptionMode;
+import com.vaadin.ui.Tree;
 
+import net.lab1318.costume.api.models.collection.CollectionEntry;
+import net.lab1318.costume.api.models.collection.CollectionId;
 import net.lab1318.costume.api.models.institution.InstitutionEntry;
-import net.lab1318.costume.api.services.institution.InstitutionQueryService;
+import net.lab1318.costume.api.services.collection.CollectionQueryService;
 import net.lab1318.costume.gui.views.TopLevelView;
 
 @SuppressWarnings("serial")
@@ -21,20 +28,37 @@ public class InstitutionsView extends TopLevelView {
         super(eventBus);
     }
 
-    public void setModels(final ImmutableList<InstitutionEntry> institutionEntries) {
-        final GridLayout rootLayout = new GridLayout();
-        for (final InstitutionEntry institutionEntry : institutionEntries) {
-            final Button institutionButton = new Button(institutionEntry.getModel().getTitle(),
-                    new Button.ClickListener() {
-                        @Override
-                        public void buttonClick(final ClickEvent event) {
-                            _getEventBus().post(new InstitutionQueryService.Messages.GetInstitutionByIdRequest(
-                                    institutionEntry.getId()));
-                        }
-                    });
-            rootLayout.addComponent(institutionButton);
+    @SuppressWarnings("unchecked")
+    public void setModels(final ImmutableMultimap<InstitutionEntry, CollectionEntry> modelTree) {
+        final Tree viewTree = new Tree();
+        viewTree.addContainerProperty("name", String.class, null);
+        viewTree.setItemCaptionMode(ItemCaptionMode.PROPERTY);
+        viewTree.setItemCaptionPropertyId("name");
+        viewTree.addItemClickListener(new ItemClickListener() {
+            @Override
+            public void itemClick(final ItemClickEvent event) {
+                if (event.getItemId() instanceof CollectionId) {
+                    _getEventBus().post(new CollectionQueryService.Messages.GetCollectionByIdRequest(
+                            (CollectionId) event.getItemId()));
+                }
+            }
+        });
+
+        for (final Map.Entry<InstitutionEntry, Collection<CollectionEntry>> modelTreeEntry : modelTree.asMap()
+                .entrySet()) {
+            final InstitutionEntry institutionEntry = modelTreeEntry.getKey();
+            final Item institutionTreeItem = viewTree.addItem(institutionEntry.getId());
+            institutionTreeItem.getItemProperty("name").setValue(institutionEntry.getModel().getTitle());
+
+            for (final CollectionEntry collectionEntry : modelTreeEntry.getValue()) {
+                final Item collectionTreeItem = viewTree.addItem(collectionEntry.getId());
+                collectionTreeItem.getItemProperty("name").setValue(collectionEntry.getModel().getTitle());
+                viewTree.setChildrenAllowed(collectionEntry.getId(), false);
+                viewTree.setParent(collectionEntry.getId(), institutionEntry.getId());
+            }
         }
-        setCompositionRoot(rootLayout);
+
+        setCompositionRoot(viewTree);
     }
 
     public final static String NAME = "institutions";
