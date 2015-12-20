067e7f8ed64b1142a555b0114ba17cd3e57dede2
minorg <minorg@server.fake>
2015-12-20 12:20:51 -0600

    py: final touchups

--- py/devbin/extract_txfc_xml.py
+++ py/devbin/extract_txfc_xml.py
@@ -1,5 +1,4 @@
 from costume.etl.txfc.txfc_extractor import TxfcExtractor
 
 
-assert __name__ == '__main__'
 TxfcExtractor.main()
--- py/devbin/load_txfc_models.py
+++ py/devbin/load_txfc_models.py
@@ -1,4 +1,3 @@
 from costume.etl.txfc.txfc_loader import TxfcLoader
 
-assert __name__ == '__main__'
 TxfcLoader.main()
