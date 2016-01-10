45e5f341578333b2868bbf9982652685800ff8ac
Minor Gordon <minorg@server.fake>
2016-01-10 14:07:12 -0600

    java: CliMain: formatting

--- java/cli/src/main/java/net/lab1318/costume/cli/CliMain.java
+++ java/cli/src/main/java/net/lab1318/costume/cli/CliMain.java
@@ -1,4 +1,3 @@
-
 package net.lab1318.costume.cli;
 
 import org.slf4j.Marker;
@@ -17,28 +16,28 @@ import net.lab1318.costume.lib.CostumeProperties;
 import net.lab1318.costume.lib.services.ServicesModule;
 
 public final class CliMain extends AbstractMain {
-    public static void main(final String[] argv) throws Exception {
-        new CliMain().__main(argv);
-    }
+	public static void main(final String[] argv) throws Exception {
+		new CliMain().__main(argv);
+	}
 
-    private CliMain() {
-        super(LOG_MARKER);
-    }
+	private CliMain() {
+		super(LOG_MARKER);
+	}
 
-    private void __main(final String[] argv) throws Exception {
-        final Args args = new Args();
-        final CommandParser argParser = new CommandParser(args, commands);
+	private void __main(final String[] argv) throws Exception {
+		final Args args = new Args();
+		final CommandParser argParser = new CommandParser(args, commands);
 
-        argParser.parse(argv);
+		argParser.parse(argv);
 
-        final CostumeProperties properties = _parseArgs(args);
+		final CostumeProperties properties = _parseArgs(args);
 
-        final Injector injector = Guice.createInjector(new PropertiesModule<CostumeProperties>(properties),
-                new ServicesModule());
+		final Injector injector = Guice.createInjector(new PropertiesModule<CostumeProperties>(properties),
+				new ServicesModule());
 
-        argParser.getCommand().run(injector);
-    }
+		argParser.getCommand().run(injector);
+	}
 
-    private final Command[] commands = { new PutElasticSearchTemplatesCommand(), new ResummarizeObjectsCommand() };
-    private final static Marker LOG_MARKER = MarkerFactory.getMarker("CLI_MAIN");
+	private final Command[] commands = { new PutElasticSearchTemplatesCommand(), new ResummarizeObjectsCommand() };
+	private final static Marker LOG_MARKER = MarkerFactory.getMarker("CLI_MAIN");
 }
