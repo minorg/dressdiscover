c7c0cf69c831558eff59a0853197bc5f3fb2edca
minorg <minorg@server.fake>
2016-02-12 15:38:34 -0600

    factored out OaiPmhExtractor, implement Europeana extractor

---
+++ py/src/costume/etl/europeana/europeana_extractor.py
@@ -0,0 +1,13 @@
+from costume.etl.oai_pmh._oai_pmh_extractor import _OaiPmhExtractor
+
+
+class EuropeanaExtractor(_OaiPmhExtractor):
+    def __init__(self, **kwds):
+        _OaiPmhExtractor.__init__(
+            self,
+            endpoint_url='http://oai.europeana.eu/oaicat/OAIHandler',
+            institution_id='europeana',
+            metadata_prefix='edm',
+            set_='2048208_Ag_EU_EuropeanaFashion_1015',
+            **kwds
+        )
