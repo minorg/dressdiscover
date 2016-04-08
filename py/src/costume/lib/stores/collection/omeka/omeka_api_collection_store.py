e69971837f630368b203e73ed76da61bdfb66357
minorg <minorg@server.fake>
2016-04-08 18:48:04 -0500

    API object store

--- py/src/costume/lib/stores/collection/omeka/omeka_api_collection_store.py
+++ py/src/costume/lib/stores/collection/omeka/omeka_api_collection_store.py
@@ -1,6 +1,8 @@
 from com.google.common.collect import ImmutableList
 
+from costume.api.services.collection.no_such_collection_exception import NoSuchCollectionException
 from costume.lib.stores.collection.omeka._omeka_collection_store import _OmekaCollectionStore
+from yomeka.api.no_such_omeka_collection_exception import NoSuchOmekaCollectionException
 from yomeka.client.omeka_rest_api_client import OmekaRestApiClient
 
 
@@ -10,7 +12,10 @@ class OmekaApiCollectionStore(_OmekaCollectionStore):
         self.__api_client = OmekaRestApiClient(api_key=api_key, endpoint_url='http://' + str(self._uri.getAuthority().get()) + (self._uri.getPath().get() if self._uri.getPath().isPresent() else ''))
 
     def getCollectionById(self, collectionId, logger, logMarker):
-        omeka_collection = self.__api_client.get_collection_by_id(int(collectionId.getUnqualifiedCollectionId()))
+        try:
+            omeka_collection = self.__api_client.get_collection(int(collectionId.getUnqualifiedCollectionId()))
+        except NoSuchOmekaCollectionException:
+            raise NoSuchCollectionException
         return \
             self._mapper.map_omeka_collection(
                 collection_store_uri=self._uri,
---
+++ py/src/costume/lib/stores/object/omeka/omeka_api_object_store.py
@@ -0,0 +1,40 @@
+from com.google.common.collect import ImmutableList
+from net.lab1318.costume.api.services.object import NoSuchObjectException
+
+from costume.lib.stores.object.omeka._omeka_object_store import _OmekaObjectStore
+from yomeka.api.no_such_omeka_item_exception import NoSuchOmekaItemException
+from yomeka.client.omeka_rest_api_client import OmekaRestApiClient
+
+
+class OmekaApiObjectStore(_OmekaObjectStore):
+    def __init__(self, api_key, uri, **kwds):
+        endpoint_url = 'http://' + str(uri.getAuthority().get()) + (uri.getPath().get() if uri.getPath().isPresent() else '')
+        _OmekaObjectStore.__init__(self, endpoint_url=endpoint_url, uri=uri, **kwds)
+        self.__api_client = OmekaRestApiClient(api_key=api_key, endpoint_url=endpoint_url)
+
+    def getObjectById(self, logger, log_marker, object_id):
+        try:
+            omeka_item = self.__api_client.get_item(id=int(object_id.getUnqualifiedObjectId()))
+        except NoSuchOmekaItemException:
+            raise NoSuchObjectException
+        return \
+            self._mapper.map_omeka_item(
+                collection_id=object_id.getCollectionId(),
+                endpoint_url=self._endpoint_url,
+                omeka_item=omeka_item,
+                omeka_item_files=self.__api_client.get_all_files(item=omeka_item.id)
+            )
+
+    def getObjectsByCollectionId(self, collection_id, logger, log_marker):
+        objects = []
+        omeka_items = self.__api_client.get_all_items(collection=int(collection_id.getUnqualifiedCollectionId()))
+        for omeka_item in omeka_items:
+            objects.append(
+                self._mapper.map_omeka_item(
+                    collection_id=collection_id,
+                    endpoint_url=self._endpoint_url,
+                    omeka_item=omeka_item,
+                    omeka_item_files=self.__api_client.get_all_files(item=omeka_item.id)
+                )
+            )
+        return ImmutableList.copyOf(objects)
