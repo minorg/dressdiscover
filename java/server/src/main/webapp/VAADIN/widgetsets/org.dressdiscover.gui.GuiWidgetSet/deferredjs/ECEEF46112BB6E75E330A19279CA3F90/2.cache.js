2d3204a8c5811e0e9926b7f751ad406bae4fd470
Minor G <minorg@server.fake>
2016-07-03 20:21:30 +0200

    recompile Vaadin widgetsets

--- java/server/src/main/webapp/VAADIN/widgetsets/org.dressdiscover.gui.GuiWidgetSet/deferredjs/ECEEF46112BB6E75E330A19279CA3F90/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.org_dressdiscover_gui_GuiWidgetSet.runAsyncCallback2('Ueb(1645,1,Fae);_.vc=function qoc(){G8b((!z8b&&(z8b=new L8b),z8b),this.a.d)};V3d(ni)(2);\n//# sourceURL=org.dressdiscover.gui.GuiWidgetSet-2.js\n')
