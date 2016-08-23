48f4c0a2ab5309599ea447a6f1b6a1cbc3baa4de
minorg <minorg@server.fake>
2016-08-23 15:55:33 -0400

    getting the TS part going

---
+++ ts/app/app.ts
@@ -0,0 +1,11 @@
+/// <reference path="../typings/index.d.ts" />
+
+import $ = require("jquery");
+import UserView = require("./view/UserView");
+import UserModel = require("./model/UserModel");
+
+$(() => {
+    var body = $("body");
+    body.html('');
+    body.append(new UserView({ model: new UserModel() }).render().$el);
+});
