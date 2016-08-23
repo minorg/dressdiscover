d04c365881040819255fa2149c619b6881aceb45
minorg <minorg@server.fake>
2016-08-23 16:37:22 -0400

    add back typings

---
+++ ts/typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="globals/backbone-global/index.d.ts" />
+/// <reference path="globals/backbone/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
+/// <reference path="globals/marionette/index.d.ts" />
+/// <reference path="globals/underscore/index.d.ts" />
