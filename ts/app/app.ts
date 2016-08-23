e3dd7edc77c03504daa17bae3a12343fb093ad6d
minorg <minorg@server.fake>
2016-08-23 16:27:17 -0400

    trying different TS plugin

--- ts/app/app.ts
+++ ts/app/app.ts
@@ -1,11 +1,11 @@
-/// <reference path="../typings/index.d.ts" />
+/// <reference path="typings/index.d.ts" />
 
 import $ = require("jquery");
-import UserView = require("./view/UserView");
-import UserModel = require("./model/UserModel");
+// import UserView = require("./view/UserView");
+// import UserModel = require("./model/UserModel");
 
 $(() => {
     var body = $("body");
     body.html('');
-    body.append(new UserView({ model: new UserModel() }).render().$el);
+    //body.append(new UserView({ model: new UserModel() }).render().$el);
 });
