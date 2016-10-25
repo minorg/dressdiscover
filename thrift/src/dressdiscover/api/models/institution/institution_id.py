e6d51f64dd15476f2a3f073db2ccd9b3816da30a
minorg <minorg@server.fake>
2016-10-25 15:55:06 -0400

    remove Python id types

--- thrift/src/dressdiscover/api/models/institution/institution_id.py
+++ thrift/src/dressdiscover/api/models/institution/institution_id.py
@@ -1,30 +1,11 @@
 import os.path, sys
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
-from _string_id import _StringId
+from _id import _Id
 
 
-class InstitutionId(_StringId):
+class InstitutionId(_Id):
     def java_read_protocol_throws_checked(self):
         return ['org.dressdiscover.api.models.institution.InvalidInstitutionIdException']
 
     def java_qname(self):
         return 'org.dressdiscover.api.models.institution.InstitutionId'
-
-    def py_qname(self):
-        return 'dressdiscover.api.models.institution.institution_id.InstitutionId'
-
-    def py_read_protocol_throws(self):
-        return ['dressdiscover.api.models.institution.InvalidInstitutionIdException']
-
-    def py_repr(self):
-        return """\
-from dressdiscover.api.models.institution.invalid_institution_id_exception import InvalidInstitutionIdException
-
-
-class InstitutionId(str):
-    @classmethod
-    def parse(cls, str_):
-        if len(str_) == 0:
-            raise InvalidInstitutionIdException("empty")
-        return cls(str_)
-"""
