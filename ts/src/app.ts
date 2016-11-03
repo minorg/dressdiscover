558e20a90ad80c6b06b3e48f0ee91c48d0cd3d90
minorg <minorg@server.fake>
2016-11-03 16:06:13 -0400

    nuking Backbone stuff

--- ts/src/app.ts
+++ ts/src/app.ts
@@ -1,9 +1,7 @@
 /// <reference path="../typings/index.d.ts" />
 
 import './css/app.css';
-import DressDiscoverApplication = require("./DressDiscoverApplication");
 
 $(() => {
-    var app = new DressDiscoverApplication.DressDiscoverApplication();
-    app.start();
+    alert("Hello world");
 });
--- ts/src/model/RootModel.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="../../typings/index.d.ts" />
-
-import Backbone = require("backbone");
-
-export class RootModel extends Backbone.Model {
-}
