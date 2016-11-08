ef44486dba7cd6cd1b662c16f27794eebcc27e4f
minorg <minorg@server.fake>
2016-11-08 15:31:47 -0500

    regenerating until compilation

--- ts/src/ts/gen/dressdiscover/api/services/worksheet/worksheet_command_service.ts
+++ ts/src/ts/gen/dressdiscover/api/services/worksheet/worksheet_command_service.ts
@@ -10,27 +10,27 @@ module dressdiscover.api.services.worksheet {
             __jsonrpc_params["worksheet_feature_set_states"] = function (value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState}): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} { var outObject: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} = {}; for (var key in value) { outObject[key] = value[key].toThryftJSON(); } return outObject; }(kwds.worksheet_feature_set_states);
 
             $.ajax({
-                async:true,
-                data:JSON.stringify({
-                    jsonrpc:'2.0',
-                    method:'put_worksheet_feature_set_states',
-                    params:__jsonrpc_params,
-                    id:'1234'
+                async: true,
+                data: JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'put_worksheet_feature_set_states',
+                    params: __jsonrpc_params,
+                    id: '1234'
                 }),
-                dataType:'json',
-                error:function(jqXHR, textStatus, errorThrown) {
-                    kwds.error(jqXHR, textStatus, errorThrown);
+                dataType: 'json',
+                error: function(jqXHR: any, textStatus: any, errorThrown: any) {
+                    kwds.error(jqXHR as JQueryXHR, textStatus as string, errorThrown as string);
                 },
-                mimeType:'application/json',
-                type:'POST',
-                success:function(__response) {
+                mimeType: 'application/json',
+                type: 'POST',
+                success: function(__response: any) {
                     if (typeof __response.result !== "undefined") {
                         kwds.success();
                     } else {
                         kwds.error(null, __response.error.message, null);
                     }
                 },
-                url:'/api/jsonrpc/worksheet_command',
+                url: '/api/jsonrpc/worksheet_command',
             });
         }
 
@@ -39,25 +39,25 @@ module dressdiscover.api.services.worksheet {
             __jsonrpc_params["worksheet_feature_set_states"] = function (value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState}): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} { var outObject: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} = {}; for (var key in value) { outObject[key] = value[key].toThryftJSON(); } return outObject; }(kwds.worksheet_feature_set_states);
 
             $.ajax({
-                async:false,
-                data:JSON.stringify({
-                    jsonrpc:'2.0',
-                    method:'put_worksheet_feature_set_states',
-                    params:__jsonrpc_params,
-                    id:'1234'
+                async: false,
+                data: JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'put_worksheet_feature_set_states',
+                    params: __jsonrpc_params,
+                    id: '1234'
                 }),
-                dataType:'json',
-                error:function(jqXHR, textStatus, errorThrown) {
+                dataType: 'json',
+                error: function(jqXHR: any, textStatus: any, errorThrown: any) {
                     throw new Error(errorThrown);
                 },
-                mimeType:'application/json',
-                type:'POST',
-                success:function(__response) {
+                mimeType: 'application/json',
+                type: 'POST',
+                success: function(__response: any) {
                     if (typeof __response.result === "undefined") {
                         throw new Error(__response.error);
                     }
                 },
-                url:'/api/jsonrpc/worksheet_command',
+                url: '/api/jsonrpc/worksheet_command',
             });
         }
     }
@@ -66,6 +66,7 @@ module dressdiscover.api.services.worksheet {
         putWorksheetFeatureSetStatesAsync(kwds: {worksheet_feature_set_states: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState}, error: (jqXHR: JQueryXHR | null, textStatus: string, errorThrown: string | null) => any, success: () => void}): void {
             try {
                 this.putWorksheetFeatureSetStatesSync({worksheet_feature_set_states: kwds.worksheet_feature_set_states});
+                kwds.success();
             } catch (e) {
                 kwds.error(null, e.message, e);
             }
--- ts/src/ts/gen/dressdiscover/api/services/worksheet/worksheet_query_service.ts
+++ ts/src/ts/gen/dressdiscover/api/services/worksheet/worksheet_query_service.ts
@@ -1,64 +1,64 @@
 module dressdiscover.api.services.worksheet {
     export interface WorksheetQueryService {
         getWorksheetFeatureSetDefinitionsAsync(kwds: {error: (jqXHR: JQueryXHR | null, textStatus: string, errorThrown: string | null) => any, success: (return_value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition}) => void}): void;
-        getWorksheetFeatureSetDefinitionsSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} | undefined;
+        getWorksheetFeatureSetDefinitionsSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition};
 
         getWorksheetFeatureSetStatesAsync(kwds: {error: (jqXHR: JQueryXHR | null, textStatus: string, errorThrown: string | null) => any, success: (return_value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState}) => void}): void;
-        getWorksheetFeatureSetStatesSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} | undefined;
+        getWorksheetFeatureSetStatesSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState};
     }
 
     export class JsonRpcWorksheetQueryService implements WorksheetQueryService {
         getWorksheetFeatureSetDefinitionsAsync(kwds: {error: (jqXHR: JQueryXHR | null, textStatus: string, errorThrown: string | null) => any, success: (return_value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition}) => void}): void {
             $.ajax({
-                async:true,
-                data:JSON.stringify({
-                    jsonrpc:'2.0',
-                    method:'get_worksheet_feature_set_definitions',
-                    params:{},
-                    id:'1234'
+                async: true,
+                data: JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'get_worksheet_feature_set_definitions',
+                    params: {},
+                    id: '1234'
                 }),
-                dataType:'json',
-                error:function(jqXHR, textStatus, errorThrown) {
-                    kwds.error(jqXHR, textStatus, errorThrown);
+                dataType: 'json',
+                error: function(jqXHR: any, textStatus: any, errorThrown: any) {
+                    kwds.error(jqXHR as JQueryXHR, textStatus as string, errorThrown as string);
                 },
-                mimeType:'application/json',
-                type:'POST',
-                success:function(__response) {
+                mimeType: 'application/json',
+                type: 'POST',
+                success: function(__response: any) {
                     if (typeof __response.result !== "undefined") {
                         kwds.success(function (json: any): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} { var map: any = {}; for (var key in json) { map[key] = dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition.fromThryftJSON(json[key]); } return map; }(__response.result));
                     } else {
                         kwds.error(null, __response.error.message, null);
                     }
                 },
-                url:'/api/jsonrpc/worksheet_query',
+                url: '/api/jsonrpc/worksheet_query',
             });
         }
 
-        getWorksheetFeatureSetDefinitionsSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} | undefined {
-            var returnValue: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} | undefined = undefined;
+        getWorksheetFeatureSetDefinitionsSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} {
+            var returnValue: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} = {};
 
             $.ajax({
-                async:false,
-                data:JSON.stringify({
-                    jsonrpc:'2.0',
-                    method:'get_worksheet_feature_set_definitions',
-                    params:{},
-                    id:'1234'
+                async: false,
+                data: JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'get_worksheet_feature_set_definitions',
+                    params: {},
+                    id: '1234'
                 }),
-                dataType:'json',
-                error:function(jqXHR, textStatus, errorThrown) {
+                dataType: 'json',
+                error: function(jqXHR: any, textStatus: any, errorThrown: any) {
                     throw new Error(errorThrown);
                 },
-                mimeType:'application/json',
-                type:'POST',
-                success:function(__response) {
+                mimeType: 'application/json',
+                type: 'POST',
+                success: function(__response: any) {
                     if (typeof __response.result !== "undefined") {
                         returnValue = function (json: any): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} { var map: any = {}; for (var key in json) { map[key] = dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition.fromThryftJSON(json[key]); } return map; }(__response.result);
                     } else {
                         throw new Error(__response.error);
                     }
                 },
-                url:'/api/jsonrpc/worksheet_query',
+                url: '/api/jsonrpc/worksheet_query',
             });
 
             return returnValue;
@@ -66,55 +66,55 @@ module dressdiscover.api.services.worksheet {
 
         getWorksheetFeatureSetStatesAsync(kwds: {error: (jqXHR: JQueryXHR | null, textStatus: string, errorThrown: string | null) => any, success: (return_value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState}) => void}): void {
             $.ajax({
-                async:true,
-                data:JSON.stringify({
-                    jsonrpc:'2.0',
-                    method:'get_worksheet_feature_set_states',
-                    params:{},
-                    id:'1234'
+                async: true,
+                data: JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'get_worksheet_feature_set_states',
+                    params: {},
+                    id: '1234'
                 }),
-                dataType:'json',
-                error:function(jqXHR, textStatus, errorThrown) {
-                    kwds.error(jqXHR, textStatus, errorThrown);
+                dataType: 'json',
+                error: function(jqXHR: any, textStatus: any, errorThrown: any) {
+                    kwds.error(jqXHR as JQueryXHR, textStatus as string, errorThrown as string);
                 },
-                mimeType:'application/json',
-                type:'POST',
-                success:function(__response) {
+                mimeType: 'application/json',
+                type: 'POST',
+                success: function(__response: any) {
                     if (typeof __response.result !== "undefined") {
                         kwds.success(function (json: any): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} { var map: any = {}; for (var key in json) { map[key] = dressdiscover.api.models.worksheet.WorksheetFeatureSetState.fromThryftJSON(json[key]); } return map; }(__response.result));
                     } else {
                         kwds.error(null, __response.error.message, null);
                     }
                 },
-                url:'/api/jsonrpc/worksheet_query',
+                url: '/api/jsonrpc/worksheet_query',
             });
         }
 
-        getWorksheetFeatureSetStatesSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} | undefined {
-            var returnValue: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} | undefined = undefined;
+        getWorksheetFeatureSetStatesSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} {
+            var returnValue: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} = {};
 
             $.ajax({
-                async:false,
-                data:JSON.stringify({
-                    jsonrpc:'2.0',
-                    method:'get_worksheet_feature_set_states',
-                    params:{},
-                    id:'1234'
+                async: false,
+                data: JSON.stringify({
+                    jsonrpc: '2.0',
+                    method: 'get_worksheet_feature_set_states',
+                    params: {},
+                    id: '1234'
                 }),
-                dataType:'json',
-                error:function(jqXHR, textStatus, errorThrown) {
+                dataType: 'json',
+                error: function(jqXHR: any, textStatus: any, errorThrown: any) {
                     throw new Error(errorThrown);
                 },
-                mimeType:'application/json',
-                type:'POST',
-                success:function(__response) {
+                mimeType: 'application/json',
+                type: 'POST',
+                success: function(__response: any) {
                     if (typeof __response.result !== "undefined") {
                         returnValue = function (json: any): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} { var map: any = {}; for (var key in json) { map[key] = dressdiscover.api.models.worksheet.WorksheetFeatureSetState.fromThryftJSON(json[key]); } return map; }(__response.result);
                     } else {
                         throw new Error(__response.error);
                     }
                 },
-                url:'/api/jsonrpc/worksheet_query',
+                url: '/api/jsonrpc/worksheet_query',
             });
 
             return returnValue;
@@ -124,22 +124,22 @@ module dressdiscover.api.services.worksheet {
     export abstract class AsyncToSyncWorksheetQueryService implements WorksheetQueryService {
         getWorksheetFeatureSetDefinitionsAsync(kwds: {error: (jqXHR: JQueryXHR | null, textStatus: string, errorThrown: string | null) => any, success: (return_value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition}) => void}): void {
             try {
-                this.getWorksheetFeatureSetDefinitionsSync();
+                kwds.success(this.getWorksheetFeatureSetDefinitionsSync());
             } catch (e) {
                 kwds.error(null, e.message, e);
             }
         }
 
-        abstract getWorksheetFeatureSetDefinitionsSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition} | undefined;
+        abstract getWorksheetFeatureSetDefinitionsSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetDefinition};
 
         getWorksheetFeatureSetStatesAsync(kwds: {error: (jqXHR: JQueryXHR | null, textStatus: string, errorThrown: string | null) => any, success: (return_value: {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState}) => void}): void {
             try {
-                this.getWorksheetFeatureSetStatesSync();
+                kwds.success(this.getWorksheetFeatureSetStatesSync());
             } catch (e) {
                 kwds.error(null, e.message, e);
             }
         }
 
-        abstract getWorksheetFeatureSetStatesSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState} | undefined;
+        abstract getWorksheetFeatureSetStatesSync(): {[index: string]: dressdiscover.api.models.worksheet.WorksheetFeatureSetState};
     }
 }
