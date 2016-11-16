d9080121616c00d5b94fea8a5f5d08b19ac8ccd1
Minor Gordon <minorg@server.fake>
2016-11-16 17:21:13 -0500

    incorporate stickit

--- ts/src/dressdiscover/gui/views/worksheet/input/worksheet_text_feature_input_view.ts
+++ ts/src/dressdiscover/gui/views/worksheet/input/worksheet_text_feature_input_view.ts
@@ -6,9 +6,36 @@ import "../../../../../../node_modules/backbone.stickit/backbone.stickit.js";
 declare function require(moduleName: string): any;
 
 export class WorksheetTextFeatureInputView extends Marionette.ItemView<WorksheetTextFeatureModel> {
+    bindings = {
+        "#text": {
+            observe: "text",
+            onSet: "onSet"
+        }
+    };
+
     constructor(options: any) {
         super(_.extend(options, {
             template: _.template(require("raw!./worksheet_text_feature_input_view.html"))
         }));
+    }
+
+    onBeforeDestroy() {
+        (this as any).unstickit();
+    }
+
+    onRender() {
+        (this as any).stickit();
+    }
+
+    onSet(value: string) {
+        for (var key in this.model.attributes) {
+            console.info(key + ' = ' + this.model.attributes[key]);
+        }
+        console.info("Set to " + value);
+        return value;
+    }
+
+    serializeData(): any {
+        return { "displayName": this.model.definition.displayName, "id": this.model.definition.id };
     }
 }
