5c77d7931163ebe58468a0c63822177bc947eb9d
minorg <minorg@server.fake>
2016-11-18 19:43:59 -0500

    checkboxes

--- ts/src/dressdiscover/gui/views/worksheet/input/worksheet_enum_feature_value_input_view.ts
+++ ts/src/dressdiscover/gui/views/worksheet/input/worksheet_enum_feature_value_input_view.ts
@@ -11,15 +11,21 @@ export class WorksheetEnumFeatureValueInputView extends Marionette.ItemView<Work
     constructor(options: { parentFeature: WorksheetEnumFeatureModel, [index: string]: any }) {    
         super(_.extend(_.omit(options, "parentFeature"), {
             events: {
+                "click input": "onClick",
                 "click a": "onClick"
             },
-            template: _.template(require("raw!./worksheet_enum_feature_value_input_view.html"))
+            template: _.template(require("raw!./worksheet_enum_feature_value_input_view.html")),
         }));
         this._parentFeature = options.parentFeature;
     }
 
+    initialize() {
+        this.ui = { checkbox: "input" };
+    }
+
     onClick() {
         this.model.selected = !this.model.selected;
+        this.ui.checkbox[0].checked = this.model.selected;
         AppRadio.channel.trigger(WorksheetFeatureInputEvent.NAME, new WorksheetFeatureInputEvent({ feature: this._parentFeature }));
     }    
 
@@ -30,6 +36,10 @@ export class WorksheetEnumFeatureValueInputView extends Marionette.ItemView<Work
         }
         data['image'] = this.model.definition.image ? this.model.definition.image.toJSON() : null;
         return data;
+    }
+
+    onRender() {
+        this.ui.checkbox[0].checked = this.model.selected;
     }
 
     private _parentFeature: WorksheetEnumFeatureModel;
