611279b965041574b2d0c45ba9c5fe68c36b4d4b
minorg <minorg@server.fake>
2016-11-20 16:48:31 -0500

    send text updates

--- ts/src/dressdiscover/gui/views/worksheet/input/worksheet_text_feature_input_view.ts
+++ ts/src/dressdiscover/gui/views/worksheet/input/worksheet_text_feature_input_view.ts
@@ -1,5 +1,7 @@
 ﻿import _ = require("underscore");
+import { AppRadio } from "dressdiscover/gui/app_radio";
 import Marionette = require("backbone.marionette");
+import { WorksheetFeatureInputEvent } from "dressdiscover/gui/events/worksheet/worksheet_feature_input_event";
 import { WorksheetTextFeatureModel } from "dressdiscover/gui/models/worksheet/worksheet_text_feature_model";
 import "../../../../../../node_modules/backbone.stickit/backbone.stickit.js";
 
@@ -16,12 +18,20 @@ export class WorksheetTextFeatureInputView extends Marionette.ItemView<Worksheet
         }));
     }
 
+    initialize() {
+        this.listenTo(this.model, "change:text", this.onTextChange);
+    }
+
     onBeforeDestroy() {
         (this as any).unstickit();
     }
 
     onRender() {
         (this as any).stickit();
+    }
+
+    onTextChange() {
+        AppRadio.channel.trigger(WorksheetFeatureInputEvent.NAME, new WorksheetFeatureInputEvent({ feature: this.model }));
     }
 
     serializeData(): any {
