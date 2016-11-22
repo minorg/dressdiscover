c6f446b152036be34e09e4e9e49dfbb9639829ea
minorg <minorg@server.fake>
2016-11-22 14:11:45 -0500

    adapt query service to use accession numbers

--- thrift/src/dressdiscover/api/services/worksheet/worksheet_query_service.thrift
+++ thrift/src/dressdiscover/api/services/worksheet/worksheet_query_service.thrift
@@ -1,10 +1,17 @@
 namespace * dressdiscover.api.services.worksheet
 
+include "dressdiscover/api/models/worksheet/worksheet_accession_number.thrift"
 include "dressdiscover/api/models/worksheet/worksheet_definition.thrift"
 include "dressdiscover/api/models/worksheet/worksheet_state.thrift"
 include "dressdiscover/api/services/io_exception.thrift"
 
 service WorksheetQueryService {
+    list<worksheet_accession_number.WorksheetAccessionNumber>
+    get_worksheet_accession_numbers(
+    ) throws (
+        io_exception.IoException e
+    );
+
     worksheet_definition.WorksheetDefinition
     get_worksheet_definition(
     ) throws (
@@ -13,6 +20,7 @@ service WorksheetQueryService {
 
     worksheet_state.WorksheetState
     get_worksheet_state(
+        worksheet_accession_number.WorksheetAccessionNumber accession_number
     ) throws (
         io_exception.IoException e
     );
