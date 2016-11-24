41d24c243dcb2c1886e9ba2d462b57432d1d9438
minorg <minorg@server.fake>
2016-11-24 12:45:43 -0500

    use min.js's

--- ts/src/dressdiscover/gui/application.ts
+++ ts/src/dressdiscover/gui/application.ts
@@ -6,12 +6,12 @@ import { Radio } from "dressdiscover/gui/radio";
 import { Router } from "dressdiscover/gui/router";
 import { ModalRegion } from "dressdiscover/gui/modal_region";
 
-import "../../../node_modules/backbone.validation/dist/backbone-validation.js";
+import "../../../node_modules/backbone.validation/dist/backbone-validation-min.js";
 import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
 import "../../../node_modules/bootstrap-select/dist/css/bootstrap-select.min.css";
 import "../../../node_modules/bootstrap-select/dist/js/bootstrap-select.min.js";
-import "../../../node_modules/bootstrap-treeview/src/css/bootstrap-treeview.css";
-import "../../../node_modules/bootstrap-treeview/src/js/bootstrap-treeview.js";
+import "../../../node_modules/bootstrap-treeview/dist/bootstrap-treeview.min.css";
+import "../../../node_modules/bootstrap-treeview/dist/bootstrap-treeview.min.js";
 
 export class Application extends Marionette.Application {
     onStart() {
