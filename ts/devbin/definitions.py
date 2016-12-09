7e20774457c7bdd375d6af98ceed2f27c1c41511
minorg <minorg@server.fake>
2016-12-09 14:46:32 -0500

    working on definitions

--- ts/devbin/definitions.py
+++ ts/devbin/definitions.py
@@ -1,24 +1,113 @@
-decoration_applied = {
-    'Decoration': {
-        'Applied': [
-            'Embroidery',
-            'Sequins',
-            'Applique',
-        ]
-    }
+# decoration_applied = {
+#     'Decoration': {
+#         'Applied': [
+#             'Embroidery',
+#             'Sequins',
+#             'Applique',
+#         ]
+#     }
+# }
+
+closures = [
+    'Button',
+    'Hook and eye',
+    'Non-functional',
+    'Zip'
+]
+
+collar_characteristics = [
+    'Attached',
+    'Bibbed',
+    'No collar',
+    'Peaked',
+    'Round',
+]
+
+colors = [
+    'Blue',
+    'Brown',
+    'Green',
+    'Red',
+    'White',
+]
+
+_vertical_extents = [
+    'Whole',
+    'Top',
+    'Bottom',
+]
+
+extents = {
+    'Whole': _vertical_extents,
+    'Collar': [],
+    'Shoulders': [],
+    'Sleeves': [],
+    'Front': _vertical_extents,
+    'Back': _vertical_extents,
+    'Interior': _vertical_extents,
 }
 
-list = [{'key1': 'value1'}, 'item', 'item1']
-string = 'item'
+interior_characteristics = [
+    'Attached underskirt',
+    'Partially lined',
+    'Lined',
+    'Unlined'
+]
 
+materials = [
+    'Cotton',
+    'Elastic',
+    'Plastic',
+    'Self-fabric',
+    'Net tulle',
+    'Velvet',
+]
 
-definitions = {
-    'Head/Neck': {
-    },
-    'Torso/Pelvis': {
-        'Main Garment (Top)': {
-            'Bodice Back': decoration_applied,
-            'Bodice Front': decoration_applied,
-        }
-    }
-}
+pleats = [
+    'Inverted',
+]
+
+prints = [
+    'Abstracted floral',
+    'Checkerboard',
+    'Floral',
+    'Plaid',
+]
+
+pocket_characteristics = [
+    'Attached',
+    'Open'
+]
+
+sleeve_characteristics = [
+    'Attached cuffs',
+    'Elbow-length',
+    'Set-in',
+    'Sleeveless',
+]
+
+techniques = [
+    'Piped trim',
+    'Gathered',
+    'Slightly gathered',
+    'Smocked',
+]
+
+waistline_characteristics = [
+    'Dropped',
+    'Natural',
+    'Undefined waistline',
+]
+
+# TODO: label as text in Whole
+
+# definitions = {
+#     'Head/Neck': {
+#     },
+#     'Torso/Pelvis': {
+#         'Main Garment (Top)': {
+#             'Bodice Back': decoration_applied,
+#             'Bodice Front': decoration_applied,
+#         }
+#     }
+# }
