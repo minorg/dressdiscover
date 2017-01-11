f82c545ed8860ee9c284ddf8c3c172c18d5adfd1
minorg <minorg@server.fake>
2017-01-11 15:38:27 -0500

    cs: redo project again

--- py/src/thryft/protocol/json_input_protocol.py
+++ py/src/thryft/protocol/json_input_protocol.py
@@ -1,5 +1,5 @@
 # -----------------------------------------------------------------------------
-# Copyright (c) 2016, Minor Gordon
+# Copyright (c) 2017, Minor Gordon
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
