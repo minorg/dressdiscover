e81c8975f0d448f0f6bedac21e2ab6144065db1d
minorg <minorg@server.fake>
2017-01-19 14:47:05 -0500

    checkpoint Services dev

---
+++ cs/DressDiscover/DressDiscover.Portable/src/DressDiscover/Gui/Portable/Services/Worksheet/LocalWorksheetCommandService.cs
@@ -0,0 +1,30 @@
+﻿using DressDiscover.Api.Models.Worksheet;
+using DressDiscover.Api.Services.Worksheet;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace DressDiscover.Gui.Portable.Services.Worksheet
+{
+    public sealed class LocalWorksheetCommandService : IWorksheetCommandService
+    {
+        public LocalWorksheetCommandService(IDictionary<string, object> properties)
+        {
+            this.properties = properties;
+        }
+
+        public void DeleteWorksheetState(string accessionNumber)
+        {
+            Debug.WriteLine("delete " + accessionNumber);
+            properties.Remove(LocalWorksheetQueryService.GetWorksheetStateItemKey(accessionNumber);
+        }
+
+        public void PutWorksheetState(WorksheetState state)
+        {
+            var stateJsonString = state.ToJson().ToString(Newtonsoft.Json.Formatting.None);
+            Debug.WriteLine("put {0} <- {1}", state.AccessionNumber, stateJsonString);
+            properties[LocalWorksheetQueryService.GetWorksheetStateItemKey(state.AccessionNumber)] = ;
+        }
+
+        private IDictionary<string, object> properties;
+    }
+}
---
+++ cs/DressDiscover/DressDiscover.Portable/src/DressDiscover/Gui/Portable/Services/Worksheet/LocalWorksheetQueryService.cs
@@ -0,0 +1,66 @@
+﻿using DressDiscover.Api.Models.Worksheet;
+using DressDiscover.Api.Services.Worksheet;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace DressDiscover.Gui.Portable.Services.Worksheet
+{
+    public sealed class LocalWorksheetQueryService : IWorksheetQueryService
+    {
+        public LocalWorksheetQueryService(IDictionary<string, object> properties, WorksheetDefinition worksheetDefinition)
+        {
+            this.properties = properties;
+            this.worksheetDefinition = worksheetDefinition;
+        }
+
+        public static string GetWorksheetStateItemKey(string accessionNumber)
+        {
+            return WORKSHEET_ITEM_KEY_PREFIX + accessionNumber;
+        }
+
+        public List<string> GetWorksheetAccessionNumbers()
+        {
+            //let result: string[] = [];
+            //for (var keyI = 0; ; keyI++)
+            //{
+            //    const key = localStorage.key(keyI);
+            //    if (key == null)
+            //    {
+            //        break;
+            //    }
+            //    if (!key.match("^" + LocalWorksheetQueryService._WORKSHEET_ITEM_KEY_PREFIX))
+            //    {
+            //        continue;
+            //    }
+            //    else if (key.length == LocalWorksheetQueryService._WORKSHEET_ITEM_KEY_PREFIX.length)
+            //    {
+            //        continue;
+            //    }
+            //    result.push(key.substr(LocalWorksheetQueryService._WORKSHEET_ITEM_KEY_PREFIX.length));
+            //}
+            //return result;
+
+            throw new NotImplementedException();
+        }
+
+        public WorksheetDefinition GetWorksheetDefinition()
+        {
+            return worksheetDefinition;
+        }
+
+        public WorksheetState GetWorksheetState(string accessionNumber)
+        {
+            var worksheetStateJson = properties[GetWorksheetStateItemKey(accessionNumber)];
+            if (!(worksheetStateJson is string))
+            {
+                return new WorksheetState(accessionNumber);
+            }
+            return WorksheetState.FromJson(JObject.Parse((string)worksheetStateJson));
+        }
+
+        private IDictionary<string, object> properties;
+        private WorksheetDefinition worksheetDefinition;
+        private static readonly string WORKSHEET_ITEM_KEY_PREFIX = "worksheet/state/";
+    }
+}
