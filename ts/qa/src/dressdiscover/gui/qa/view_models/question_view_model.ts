11e8b12cbb2edd4ad33e8bec7531bd1690e6f6c6
Minor G <minorg@server.fake>
2017-02-17 18:02:01 -0500

    previous/next buttons

--- ts/qa/src/dressdiscover/gui/qa/view_models/question_view_model.ts
+++ ts/qa/src/dressdiscover/gui/qa/view_models/question_view_model.ts
@@ -7,10 +7,37 @@ import { ViewModel } from "dressdiscover/gui/qa/view_models/view_model";
 export class QuestionViewModel extends ViewModel {
     constructor(public readonly object: QaObject, public readonly questionSet: QuestionSet, public readonly questions: Question[], public readonly currentQuestion?: Question) {
         super();
+        if (currentQuestion) {
+            for (this._currentQuestionIndex = 0; this._currentQuestionIndex < questions.length; this._currentQuestionIndex++) {
+                if (currentQuestion.id == questions[this._currentQuestionIndex].id) {
+                    break;
+                }
+            }
+        }
     }
 
-    onClickQuestion(self, question) {
+    get nextQuestionButtonEnabled() {
+        return this._currentQuestionIndex && this._currentQuestionIndex + 1 < this.questions.length;
+    }
+    
+    onClickNextQuestionButton() {
+        Application.instance.router.goToQuestion(this.object.id, this.questions[this._currentQuestionIndex + 1].id, this.questionSet.id);
+        return false;
+    }
+
+    onClickQuestionLink(self, question) {
         Application.instance.router.goToQuestion(self.object.id, question.id, self.questionSet.id);
         return false;
     }
+
+    onClickPreviousQuestionButton() {
+        Application.instance.router.goToQuestion(this.object.id, this.questions[this._currentQuestionIndex - 1].id, this.questionSet.id);
+        return false;
+    }
+
+    get previousQuestionButtonEnabled() {
+        return this._currentQuestionIndex && this._currentQuestionIndex > 0;
+    }
+
+    private _currentQuestionIndex: number | undefined;
 }
