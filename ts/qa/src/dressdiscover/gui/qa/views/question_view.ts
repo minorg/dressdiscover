4103d688e1dc55f0e3a1f81bf3e943df112c6fc6
Minor G <minorg@server.fake>
2017-02-17 17:45:36 -0500

    show object on question page

--- ts/qa/src/dressdiscover/gui/qa/views/question_view.ts
+++ ts/qa/src/dressdiscover/gui/qa/views/question_view.ts
@@ -1,9 +1,10 @@
 ﻿import { TopLevelView } from "dressdiscover/gui/qa/views/top_level_view";
 import { QuestionViewModel } from "dressdiscover/gui/qa/view_models/question_view_model";
 import "./question_view.less";
+import "./templates/image_rights_template.less";
 
 export class QuestionView extends TopLevelView<QuestionViewModel> {
     constructor(viewModel: QuestionViewModel) {
-        super("question_view.html", [], viewModel);
+        super("question_view.html", ["templates/image_rights_template.html"], viewModel);
     }
 }
