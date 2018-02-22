34554af486a1c5b0a3614ceb8bfb5da037651395
Minor G <minorg@server.fake>
2018-02-21 20:43:35 -0600

    delete cs/

--- cs/DressDiscover/DressDiscover.Portable/src/DressDiscover/Api/Services/Worksheet/IWorksheetCommandService.cs
+++
@@ -1,8 +0,0 @@
-namespace DressDiscover.Api.Services.Worksheet
-{
-    public interface IWorksheetCommandService
-    {
-        void DeleteWorksheetState(string accessionNumber);
-        void PutWorksheetState(DressDiscover.Api.Models.Worksheet.WorksheetState state);
-    }
-}
--- cs/DressDiscover/DressDiscover.Portable/src/DressDiscover/Api/Services/Worksheet/IWorksheetQueryService.cs
+++
@@ -1,9 +0,0 @@
-namespace DressDiscover.Api.Services.Worksheet
-{
-    public interface IWorksheetQueryService
-    {
-        System.Collections.Generic.IList<string> GetWorksheetAccessionNumbers();
-        DressDiscover.Api.Models.Worksheet.WorksheetDefinition GetWorksheetDefinition();
-        DressDiscover.Api.Models.Worksheet.WorksheetState GetWorksheetState(string accessionNumber);
-    }
-}
