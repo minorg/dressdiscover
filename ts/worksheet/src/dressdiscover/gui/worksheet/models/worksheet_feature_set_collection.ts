eb8d91d6ce24457e8f0e94ab73962934c7723c09
Minor G <minorg@server.fake>
2018-02-23 10:58:20 -0600

    clear out existing code

--- ts/worksheet/src/dressdiscover/gui/worksheet/models/worksheet_feature_set_collection.ts
+++
@@ -1,8 +0,0 @@
-﻿import Backbone = require("backbone");
-import { WorksheetFeatureSet } from "dressdiscover/gui/worksheet/models/worksheet_feature_set";
-
-export class WorksheetFeatureSetCollection extends Backbone.Collection<WorksheetFeatureSet> {
-    constructor(models: WorksheetFeatureSet[]) {
-        super(models);
-    }
-}
--- ts/worksheet/src/dressdiscover/gui/worksheet/views/navbar_view.ts
+++
@@ -1,9 +0,0 @@
-﻿import _ = require("underscore");
-import Backbone = require("backbone");
-import Marionette = require("backbone.marionette");
-
-declare function require(moduleName: string): any;
-
-export class NavbarView extends Marionette.ItemView<Backbone.Model> {
-    template: any = _.template(require("raw!./navbar_view.html"));
-}
