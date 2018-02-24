49f19b88251f12301520728401f09f8a6dddee07
Minor G <minorg@server.fake>
2018-02-24 11:42:19 -0600

    validate extents

--- ts/worksheet/src/dressdiscover/gui/worksheet/models/worksheet_definition_validator.ts
+++ ts/worksheet/src/dressdiscover/gui/worksheet/models/worksheet_definition_validator.ts
@@ -1,10 +1,21 @@
 import { WorksheetDefinition } from 'dressdiscover/api/models/worksheet/worksheet_definition';
+import { WorksheetExtentDefinition } from 'dressdiscover/api/models/worksheet/worksheet_extent_definition';
 import { WorksheetFeatureDefinition } from 'dressdiscover/api/models/worksheet/worksheet_feature_definition';
 import { WorksheetFeatureValueDefinition } from 'dressdiscover/api/models/worksheet/worksheet_feature_value_definition';
 import * as _ from 'underscore';
 
 export class WorksheetDefinitionValidator {
     validateWorksheetDefinition(definition: WorksheetDefinition): WorksheetDefinition {
+        const extentsById: { [index: string]: WorksheetExtentDefinition } = {};
+        if (definition.extents) {
+            for (let extent of definition.extents) {
+                if (!_.isUndefined(extentsById[extent.id])) {
+                    throw new RangeError("duplicate extent id " + extent.id);
+                }
+                extentsById[extent.id] = extent;
+            }
+        }
+
         const featuresById: { [index: string]: WorksheetFeatureDefinition } = {};
         for (let feature of definition.features) {
             if (!_.isUndefined(featuresById[feature.id])) {
@@ -22,6 +33,14 @@ export class WorksheetDefinitionValidator {
         }
 
         for (let featureSet of definition.featureSets) {
+            if (featureSet.extentIds) {
+                for (let extentId of featureSet.extentIds) {
+                    if (_.isUndefined(extentsById[extentId])) {
+                        throw new RangeError("feature set " + featureSet.id + " references unknown extent id " + extentId);
+                    }
+                }
+            }
+
             for (let featureId of featureSet.featureIds) {
                 if (_.isUndefined(featuresById[featureId])) {
                     throw new RangeError("feature set " + featureSet.id + " references unknown feature id " + featureId);
@@ -30,6 +49,14 @@ export class WorksheetDefinitionValidator {
         }
 
         for (let feature of definition.features) {
+            if (feature.extentIds) {
+                for (let extentId of feature.extentIds) {
+                    if (_.isUndefined(extentsById[extentId])) {
+                        throw new RangeError("feature " + feature.id + " references unknown extent id " + extentId);
+                    }
+                }
+            }
+
             for (let featureValueId of feature.valueIds) {
                 if (_.isUndefined(featureValuesById[featureValueId])) {
                     throw new RangeError("feature " + feature.id + " references unknown feature " + featureValueId);
