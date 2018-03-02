dc3c9e6b62ce6738a8221f27713882f0d2f52adc
Minor G <minorg@server.fake>
2018-03-02 10:34:58 -0600

    add feature description parse to CSV

--- ts/worksheet/webpack.common.js
+++ ts/worksheet/webpack.common.js
@@ -49,7 +49,7 @@ module.exports = {
     })
   ],
   resolve: {
-    extensions: ['.js', '.ts'],
+    extensions: ['.ts', '.js'],
     modules: [path.join(__dirname, 'src'), path.join(__dirname, 'node_modules')]
   },
   resolveLoader: {
