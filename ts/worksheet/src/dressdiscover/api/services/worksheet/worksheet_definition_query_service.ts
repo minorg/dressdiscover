c9e611eec5ecb80ee6c5e6c01b3954a2253348dd
Minor G <minorg@server.fake>
2018-03-10 09:37:42 -0600

    regen with jsdoc

--- ts/worksheet/src/dressdiscover/api/services/worksheet/worksheet_definition_query_service.ts
+++ ts/worksheet/src/dressdiscover/api/services/worksheet/worksheet_definition_query_service.ts
@@ -1,6 +1,16 @@
 import { WorksheetDefinition } from "../../models/worksheet/worksheet_definition";
 
 export interface WorksheetDefinitionQueryService {
+    /**
+     * getWorksheetDefinition
+     * @return {WorksheetDefinition}
+     * @throws {IoException}
+     */
     getWorksheetDefinitionAsync(kwds: {error: (errorKwds: {textStatus: string, errorThrown: any, [index: string]: any}) => any, success: (returnValue: WorksheetDefinition) => void}): void;
+    /**
+     * getWorksheetDefinition
+     * @return {WorksheetDefinition}
+     * @throws {IoException}
+     */
     getWorksheetDefinitionSync(): WorksheetDefinition;
 }
--- ts/worksheet/src/dressdiscover/api/services/worksheet/worksheet_state_query_service.ts
+++ ts/worksheet/src/dressdiscover/api/services/worksheet/worksheet_state_query_service.ts
@@ -2,9 +2,33 @@ import { WorksheetState } from "../../models/worksheet/worksheet_state";
 import { WorksheetStateId } from "../../models/worksheet/worksheet_state_id";
 
 export interface WorksheetStateQueryService {
+    /**
+     * getWorksheetState
+     * @param {WorksheetStateId} id
+     * @return {WorksheetState}
+     * @throws {IoException}
+     * @throws {NoSuchWorksheetStateException}
+     */
     getWorksheetStateAsync(kwds: {id: WorksheetStateId, error: (errorKwds: {textStatus: string, errorThrown: any, [index: string]: any}) => any, success: (returnValue: WorksheetState) => void}): void;
+    /**
+     * getWorksheetState
+     * @param {WorksheetStateId} id
+     * @return {WorksheetState}
+     * @throws {IoException}
+     * @throws {NoSuchWorksheetStateException}
+     */
     getWorksheetStateSync(kwds: {id: WorksheetStateId}): WorksheetState;
 
+    /**
+     * getWorksheetStateIds
+     * @return {WorksheetStateId[]}
+     * @throws {IoException}
+     */
     getWorksheetStateIdsAsync(kwds: {error: (errorKwds: {textStatus: string, errorThrown: any, [index: string]: any}) => any, success: (returnValue: WorksheetStateId[]) => void}): void;
+    /**
+     * getWorksheetStateIds
+     * @return {WorksheetStateId[]}
+     * @throws {IoException}
+     */
     getWorksheetStateIdsSync(): WorksheetStateId[];
 }
