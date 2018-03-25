b0f0ce01eaf9d9ae6c06a8f5fd12ff4e0ed5a356
Minor G <minorg@server.fake>
2018-03-25 16:17:20 -0500

    add ping testing service

---
+++ thrift/src/dressdiscover/api/services/worksheet/worksheet_ping_query_service.thrift
@@ -0,0 +1,7 @@
+namespace controller_cs DressDiscover.Server.Controllers.Worksheet
+namespace cs DressDiscover.Api.Services.Worksheet
+namespace * dressdiscover.api.services.worksheet
+
+service WorksheetPingQueryService {
+    string ping(string message);
+}
