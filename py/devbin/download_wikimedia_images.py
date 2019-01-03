d19e39fd2dd453789c0d1e1c34aa38c00f722005
Minor G <minorg@server.fake>
2019-01-03 16:53:17 -0600

    script to download images

---
+++ py/devbin/download_wikimedia_images.py
@@ -0,0 +1,52 @@
+import argparse
+import base64
+import csv
+import os.path
+import pathvalidate
+from urllib.parse import urlparse
+from urllib.request import unquote, urlopen
+
+
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument("csv_file", help="CSV file with URLs in the first column")
+args = argument_parser.parse_args()
+
+
+ROOT_DIR_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+IMAGE_DIR_PATH = os.path.join(ROOT_DIR_PATH, "ts", "worksheet", "wwwroot", "img")
+UNSORTED_IMAGES_DIR_PATH = os.path.join(IMAGE_DIR_PATH, "unsorted_full_size")
+if not os.path.isdir(UNSORTED_IMAGES_DIR_PATH):
+    os.mkdir(UNSORTED_IMAGES_DIR_PATH)
+
+
+with open(args.csv_file) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row_i, row in enumerate(csv_reader):
+        if row_i == 0:
+            continue
+        elif not row:
+            continue
+        url = row[0].strip()
+        if not url:
+            continue
+
+        parsed_url = urlparse(url)
+        url_file_name = unquote(parsed_url.path.split('/')[-1]).encode("ascii", "replace").decode("ascii")
+        image_file_name = pathvalidate.sanitize_filename(url_file_name)
+        image_file_stem, image_file_ext = os.path.splitext(image_file_name)
+        image_file_name = pathvalidate.replace_symbol(image_file_stem, '_') + image_file_ext
+        image_file_path = os.path.join(UNSORTED_IMAGES_DIR_PATH, image_file_name)
+        if os.path.isfile(image_file_path):
+            # print(image_file_path, "already exists, skipping")
+            continue
+
+        url_obj = urlopen(url)
+        try:
+            url_contents = url_obj.read()
+        finally:
+            url_obj.close()
+
+        with open(image_file_path, "w+b") as image_file:
+            image_file.write(url_contents)
+        # print("downloaded", url, "to", image_file_path)
+        print(image_file_name)
