47c899ef3874c46e33db8f2cf7322695233a8190
Minor G <minorg@server.fake>
2019-01-07 19:51:11 -0600

    reworking worksheet state table

--- ts/worksheet/src/ts/dressdiscover/gui/worksheet/components/worksheet/WorksheetStatesTable.tsx
+++ ts/worksheet/src/ts/dressdiscover/gui/worksheet/components/worksheet/WorksheetStatesTable.tsx
@@ -14,9 +14,9 @@ interface Props {
 export class WorksheetStatesTable extends React.Component<Props> {
     render() {
         return (
-            <Table className="table table-bordered table-striped w-100">
+            <Table className="table table-bordered w-100 worksheet-states">
                 <tbody>
-                    {this.props.worksheetStateStore!.worksheetStateIds.map(worksheetStateId =>
+                    {this.props.worksheetStateStore!.worksheetStateIds!.map(worksheetStateId =>
                         <WorksheetStatesTableRow onDeleteWorksheetState={this.props.worksheetStateStore!.deleteWorksheetState.bind(this.props.worksheetStateStore)} onRenameWorksheetState={this.props.worksheetStateStore!.renameWorksheetState.bind(this.props.worksheetStateStore)} worksheetStateId={worksheetStateId}></WorksheetStatesTableRow>
                     )}
                 </tbody>
--- ts/worksheet/src/ts/dressdiscover/gui/worksheet/components/worksheet/WorksheetStatesTableRow.tsx
+++ ts/worksheet/src/ts/dressdiscover/gui/worksheet/components/worksheet/WorksheetStatesTableRow.tsx
@@ -1,3 +1,5 @@
+import './WorksheetStatesTableRow.scss';
+
 import * as classnames from 'classnames';
 import { WorksheetStateId } from 'dressdiscover/api/models/worksheet/worksheet_state_id';
 import { WorksheetStateMark } from 'dressdiscover/api/models/worksheet/worksheet_state_mark';
@@ -78,32 +80,52 @@ export class WorksheetStatesTableRow extends React.Component<Props, State> {
     }
 
     render() {
-        return (
-            <tr>
-                <td className="align-middle w-80">
-                    {this.state.renaming ? (
-                        <React.Fragment>
-                            <Input className="form-control rename-input" onChange={this.onChangeNewId.bind(this)} onKeyPress={this.onKeypressNewId.bind(this)} placeholder="Rename list" value={this.state.newId} style={{ display: "inline-block", width: 'auto' }} type="text" />
-                            <Button className="cancel-rename-button" color="default" onClick={this.onClickCancelButton.bind(this)}>Cancel</Button>
-                            <Button className={classnames({ "confirm-rename-button": true, invisible: !this.renameConfirmButtonEnabled, visible: !this.onClickRenameConfirmButton })} color="danger" onClick={this.onClickRenameConfirmButton}>Confirm</Button>
-                        </React.Fragment>
-                    ) : (
-                            <React.Fragment>
-                                <Link to={Hrefs.worksheetState(new WorksheetStateMark({ worksheetStateId: this.props.worksheetStateId }))} title="Open this worksheet">{this.props.worksheetStateId.toString()}</Link>
-                                <div className={classnames({ "float-right": true, invisible: !this.state.deleting, visible: !!this.state.deleting })}>
-                                    <span>Delete?</span>
-                                    <Button className="confirm-delete-button" color="danger" onClick={this.onClickDeleteConfirmButton.bind(this)} size="sm">Yes</Button>
-                                    <Button className="cancel-delete-button" color="default" onClick={this.onClickCancelButton.bind(this)} size="sm">No</Button>
-                                </div>
-                            </React.Fragment>
-                        )}
-                </td>
-                <td className="delete-button text-center">
-                    <Button onClick={this.onClickDeleteButton.bind(this)} title="Delete this worksheet"><i className="fas fa-trash-alt"></i></Button>
-                </td>
-                <td className="rename-button text-center">
-                    <Button onClick={this.onClickRenameButton.bind(this)} title="Rename this worksheet"><i className="fas fa-pencil-alt"></i></Button>
-                </td>
-            </tr>);
+        if (this.state.deleting) {
+            return (
+                <tr>
+                    <td className="id leftmost">
+                        {this.props.worksheetStateId.toString()}
+                    </td>
+                    <td className="inner prompt text-danger">
+                        <span>Delete?</span>
+                    </td>
+                    <td className="cancel inner">
+                        <Button className="cancel-delete-button" color="primary" onClick={this.onClickCancelButton.bind(this)} size="sm">No</Button>
+                    </td>
+                    <td className="confirm rightmost">
+                        <Button className="confirm-delete-button" color="danger" onClick={this.onClickDeleteConfirmButton.bind(this)} size="sm">Yes</Button>
+                    </td>
+                </tr>);
+        } else if (this.state.renaming) {
+            return (
+                <tr>
+                    <td className="id leftmost">
+                        <Input autoFocus className="form-control rename-input w-100" onChange={this.onChangeNewId.bind(this)} onKeyPress={this.onKeypressNewId.bind(this)} placeholder="Rename list" value={this.state.newId} style={{ display: "inline-block", width: 'auto' }} type="text" />
+                    </td>
+                    <td className="inner prompt">&nbsp;</td>
+                    <td className="cancel inner">
+                        <Button className="cancel-rename-button" color="primary" onClick={this.onClickCancelButton.bind(this)}>Cancel</Button>
+                    </td>
+                    <td className="confirm rightmost">
+                        <Button className={classnames({ "confirm-rename-button": true, invisible: !this.renameConfirmButtonEnabled, visible: this.renameConfirmButtonEnabled })} color="danger" onClick={this.onClickRenameConfirmButton.bind(this)}>Confirm</Button>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>);
+        } else {
+            return (
+                <tr>
+                    <td className="id leftmost">
+                        <Link to={Hrefs.worksheetState(new WorksheetStateMark({ worksheetStateId: this.props.worksheetStateId }))} title="Open this worksheet">{this.props.worksheetStateId.toString()}</Link>
+                    </td>
+                    <td className="inner prompt">&nbsp;</td>
+                    <td className="delete-button inner">
+                        <Button onClick={this.onClickDeleteButton.bind(this)} title="Delete this worksheet"><i className="fas fa-trash-alt"></i></Button>
+                    </td>
+                    <td className="rename-button rightmost">
+                        <Button onClick={this.onClickRenameButton.bind(this)} title="Rename this worksheet"><i className="fas fa-pencil-alt"></i></Button>
+                    </td>
+                </tr>
+            );
+        }
     }
 }
