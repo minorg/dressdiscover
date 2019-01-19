20ab80b2a10277576a581d955c2fb35e3481b228
Minor G <minorg@server.fake>
2019-01-18 21:51:40 -0600

    regen id classes with ===

--- ts/worksheet/src/ts/dressdiscover/api/models/worksheet/worksheet_feature_value_id.ts
+++ ts/worksheet/src/ts/dressdiscover/api/models/worksheet/worksheet_feature_value_id.ts
@@ -1,6 +1,6 @@
 export class WorksheetFeatureValueId {
     public static parse(id: string): WorksheetFeatureValueId {
-        if (id.trim().length == 0) {
+        if (id.trim().length === 0) {
             throw new RangeError("id is empty");
         }
         return new WorksheetFeatureValueId(id);
@@ -10,16 +10,16 @@ export class WorksheetFeatureValueId {
         if (typeof(other) === "undefined") {
             return false;
         }
-        return this._id == other._id;
+        return this.id === other.id;
     }
 
     public toString(): string {
-        return this._id;
+        return this.id;
     }
 
     private constructor(id: string) {
-        this._id = id;
+        this.id = id;
     }
 
-    private _id: string;
+    private id: string;
 }
