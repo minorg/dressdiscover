ce1ee686576a6773eb84627a4d990e7cd991c118
Minor G <minorg@server.fake>
2019-01-19 19:26:44 -0600

    WorksheetFeatureSetStateTable: clean up, appease tslint

--- ts/worksheet/src/ts/dressdiscover/gui/worksheet/components/worksheet/WorksheetFeatureSetStateTable.tsx
+++ ts/worksheet/src/ts/dressdiscover/gui/worksheet/components/worksheet/WorksheetFeatureSetStateTable.tsx
@@ -1,8 +1,7 @@
 import { WorksheetFeatureSetState } from 'dressdiscover/api/models/worksheet/worksheet_feature_set_state';
 import { WorksheetFeatureState } from 'dressdiscover/api/models/worksheet/worksheet_feature_state';
-import { WorksheetStateId } from 'dressdiscover/api/models/worksheet/worksheet_state_id';
 import { WorksheetStateMark } from 'dressdiscover/api/models/worksheet/worksheet_state_mark';
-import * as _ from 'lodash';
+import { WorksheetStateWrapper } from 'dressdiscover/gui/worksheet/models/worksheet/WorksheetStateWrapper';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
 import { Table } from 'reactstrap';
@@ -18,32 +17,22 @@ interface Props {
     featureSetDefinition: WorksheetFeatureSetDefinitionWrapper;
     featureSetState?: WorksheetFeatureSetState;
     includeFeatureDescriptions: boolean;
-    worksheetDefinition: WorksheetDefinitionWrapper;
-    worksheetStateId: WorksheetStateId;
+    worksheetState: WorksheetStateWrapper;
 }
 
 export class WorksheetFeatureSetStateTable extends React.Component<Props> {
     render() {
-        const rows: WorksheetFeatureSetStateTableRow[] = [];
-        for (let featureDefinition of this.props.featureSetDefinition.features) {
-            let featureState: WorksheetFeatureState | undefined = undefined;
-            if (this.props.featureSetState) {
-                for (let checkFeatureState of this.props.featureSetState.features) {
-                    if (checkFeatureState.id.equals(featureDefinition.id)) {
-                        featureState = checkFeatureState;
-                        break;
-                    }
-                }
-            }
+        const rows = this.props.featureSetDefinition.features.map((featureDefinition) => {
+            const featureState: WorksheetFeatureState | undefined = (this.props.featureSetState) ? this.props.featureSetState.features.find((featureState) => featureState.id.equals(featureDefinition.id)) : undefined;
 
             const featureStateMark = new WorksheetStateMark({
                 featureId: featureDefinition.id,
                 featureSetId: this.props.featureSetDefinition.id,
-                worksheetStateId: this.props.worksheetStateId
+                worksheetStateId: this.props.worksheetState.id
             });
 
-            rows.push(new WorksheetFeatureSetStateTableRow(featureDefinition, featureStateMark, this.props.worksheetDefinition, featureState))
-        }
+            return new WorksheetFeatureSetStateTableRow(featureDefinition, featureStateMark, this.props.worksheetState.worksheetDefinition, featureState);
+        });
 
         return (
             <Table className="table-bordered">
@@ -56,9 +45,9 @@ export class WorksheetFeatureSetStateTable extends React.Component<Props> {
                     </tr>
                 </thead>
                 <tbody>
-                    {_.map(rows, (row) => {
+                    {rows.map((row) => {
                         return (
-                            <tr>
+                            <tr key={row.featureDefinition.id.toString()}>
                                 <td className="text-center w-25">
                                     <Link className="btn btn-lg btn-secondary w-100" to={Hrefs.worksheetState(row.featureStateMark)}>{row.featureDefinition.displayName}</Link>
                                 </td>
@@ -67,9 +56,9 @@ export class WorksheetFeatureSetStateTable extends React.Component<Props> {
                                         {row.featureDefinition.description ? <WorksheetDescriptionComponent description={row.featureDefinition.description} /> : null}
                                     </td>) : null}
                                 <td className="align-middle">
-                                    {_.map(row.values, (value) => {
-                                        <span className="border border-info h4 m-2 p-2" data-bind="text: displayName" style={{ borderWidth: "8px !important" }}>{value.displayName}</span>
-                                    })}
+                                    {row.values.map((value) =>
+                                        <span className="border border-info h4 m-2 p-2" key={value.id.toString()} style={{ borderWidth: "8px !important" }}>{value.displayName}</span>
+                                    )}
                                 </td>
                             </tr>
                         );
@@ -83,7 +72,7 @@ export class WorksheetFeatureSetStateTable extends React.Component<Props> {
 class WorksheetFeatureSetStateTableRow {
     constructor(readonly featureDefinition: WorksheetFeatureDefinitionWrapper, readonly featureStateMark: WorksheetStateMark, worksheetDefinition: WorksheetDefinitionWrapper, featureState?: WorksheetFeatureState, ) {
         if (featureState && featureState.selectedValueIds) {
-            for (let valueId of featureState.selectedValueIds) {
+            for (const valueId of featureState.selectedValueIds) {
                 this.values.push(worksheetDefinition.getFeatureValueById(valueId));
             }
         }
