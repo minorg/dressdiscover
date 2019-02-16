9cb4c727a9c942fb34312baad5b5331e1fd9574d
Minor G <minorg@server.fake>
2019-02-16 16:44:02 -0600

    final tweaks

--- ts/src/ts/dressdiscover/gui/components/login/Login.tsx
+++ ts/src/ts/dressdiscover/gui/components/login/Login.tsx
@@ -9,7 +9,7 @@ import * as React from 'react';
 export class Login extends React.Component<{ currentUserStore: CurrentUserStore }> {
     componentDidMount() {
         const query = queryString.stringify({
-            access_type: "offline",
+            // access_type: "offline",
             client_id: Secrets.GOOGLE_CLIENT_ID,
             redirect_uri: window.location.protocol + "//" + window.location.host + Hrefs.loginCallback,
             response_type: "token",
