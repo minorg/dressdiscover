22161f81390b64c699beda7607e5013002765ae7
Minor G <minorg@server.fake>
2019-02-17 17:43:40 -0600

    first cut of Sheets query service

--- ts/src/ts/dressdiscover/gui/services/worksheet/state/GoogleSheetsWorksheetStateQueryService.ts
+++ ts/src/ts/dressdiscover/gui/services/worksheet/state/GoogleSheetsWorksheetStateQueryService.ts
@@ -1,32 +1,98 @@
-﻿// export class GoogleSheetsWorksheetStateQueryService implements WorksheetStateQueryService {
-//     constructor(private readonly configuration: GoogleSheetsWorksheetStateConfiguration) {
-//     }
+﻿import {
+    GoogleSheetsWorksheetStateConfiguration,
+} from 'dressdiscover/api/models/worksheet/configuration/google_sheets_worksheet_state_configuration';
+import { WorksheetStateId } from 'dressdiscover/api/models/worksheet/state/worksheet_state_id';
+import { IoException } from 'dressdiscover/api/services/io_exception';
+import { NoSuchWorksheetStateException } from 'dressdiscover/api/services/worksheet/state/no_such_worksheet_state_exception';
+import { WorksheetStateQueryService } from 'dressdiscover/api/services/worksheet/state/worksheet_state_query_service';
+import { CsvWorksheetStateImporter } from 'dressdiscover/gui/components/worksheet/state/importers/CsvWorksheetStateImporter';
+import * as _ from 'lodash';
 
-//     // getWorksheetState(kwds: { id: WorksheetStateId; }): Promise<WorksheetState> {
-//     //     return new Promise((resolve, reject) => {
-//     //         const jsonString = localStorage.getItem(LocalStorageWorksheetStateQueryService.getWorksheetStateItemKey(kwds.id));
-//     //         if (jsonString == null) {
-//     //             reject(new NoSuchWorksheetStateException({ id: kwds.id }));
-//     //             return;
-//     //         }
-//     //         resolve(WorksheetState.fromThryftJsonObject(JSON.parse(jsonString)));
-//     //     });
-//     // }
+export class GoogleSheetsWorksheetStateQueryService implements WorksheetStateQueryService {
+    constructor(private readonly configuration: GoogleSheetsWorksheetStateConfiguration) {
+    }
 
-//     // getWorksheetStateIds(): Promise<WorksheetStateId[]> {
-//     //     let result: WorksheetStateId[] = [];
-//     //     for (var keyI = 0; ; keyI++) {
-//     //         const key = localStorage.key(keyI);
-//     //         if (key == null) {
-//     //             break;
-//     //         }
-//     //         if (!key.match("^" + LocalStorageWorksheetStateQueryService._WORKSHEET_ITEM_KEY_PREFIX)) {
-//     //             continue;
-//     //         } else if (key.length == LocalStorageWorksheetStateQueryService._WORKSHEET_ITEM_KEY_PREFIX.length) {
-//     //             continue;
-//     //         }
-//     //         result.push(WorksheetStateId.parse(key.substr(LocalStorageWorksheetStateQueryService._WORKSHEET_ITEM_KEY_PREFIX.length)));
-//     //     }
-//     //     return new Promise((resolve, reject) => resolve(result));
-//     // }
-// }
+    static getFirstSheetData(kwds: { spreadsheetId: string }): Promise<string[][]> {
+        return new Promise((resolve, reject) => {
+            gapi.client.spreadsheets.get(kwds).then((response) => {
+                const sheets = response.result.sheets;
+                if (!sheets || !sheets.length) {
+                    resolve([]);
+                    return;
+                }
+
+                const firstSheet = sheets[0];
+                if (!firstSheet.data) {
+                    resolve([]);
+                    return;
+                }
+
+                const firstSheetData = firstSheet.data[0];
+
+                const rowData = firstSheetData.rowData;
+                if (!rowData) {
+                    resolve([]);
+                    return;
+                }
+
+                const stringRows: string[][] = [];
+                for (const rowDatum of rowData) {
+                    if (!rowDatum.values) {
+                        continue;
+                    }
+                    const stringRow: string[] = [];
+                    for (const cellDatum of rowDatum.values) {
+                        const value = cellDatum.effectiveValue;
+                        let stringValue: string;
+                        if (!value) {
+                            stringValue = "";
+                        } else if (!_.isUndefined(value.boolValue)) {
+                            stringValue = value.boolValue ? "true" : "false";
+                        } else if (!_.isUndefined(value.errorValue)) {
+                            stringValue = "error";
+                        } else if (!_.isUndefined(value.formulaValue)) {
+                            stringValue = "formula";
+                        } else if (!_.isUndefined(value.numberValue)) {
+                            stringValue = value.numberValue.toString();
+                        } else if (!_.isUndefined(value.stringValue)) {
+                            stringValue = value.stringValue;
+                        } else {
+                            stringValue = "";
+                        }
+                        stringRow.push(stringValue);
+                    }
+                    stringRows.push(stringRow);
+                }
+                resolve(stringRows);
+            }, (reason: any) => {
+                reject(new IoException(reason.toString()));
+            });
+        });
+    }
+
+    getWorksheetState(kwds: { id: WorksheetStateId; }): Promise<WorksheetState> {
+        const spreadsheetId = this.configuration.spreadsheetId;
+        return new Promise((resolve, reject) => {
+            GoogleSheetsWorksheetStateQueryService.getFirstSheetData({ spreadsheetId }).then((stringRows) => {
+                const worksheetStates = new CsvWorksheetStateImporter().importCsvRows(stringRows);
+                for (const worksheetState of worksheetStates) {
+                    if (worksheetState.id.equals(kwds.id)) {
+                        resolve(worksheetState);
+                        return;
+                    }
+                }
+                reject(new NoSuchWorksheetStateException(kwds));
+            }, (reason: any) => reject(reason));
+        });
+    }
+
+    getWorksheetStateIds(): Promise<WorksheetStateId[]> {
+        const spreadsheetId = this.configuration.spreadsheetId;
+        return new Promise((resolve, reject) => {
+            GoogleSheetsWorksheetStateQueryService.getFirstSheetData({ spreadsheetId }).then((stringRows) => {
+                const worksheetStates = new CsvWorksheetStateImporter().importCsvRows(stringRows);
+                return worksheetStates.map((worksheetState) => worksheetState.id);
+            }, (reason: any) => reject(reason));
+        });
+    }
+}
