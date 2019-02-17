afd8083c4b5fb0b1d9b730a289434bd6d548ae14
Minor G <minorg@server.fake>
2019-02-17 11:12:13 -0600

    start of new services

---
+++ ts/src/ts/dressdiscover/gui/services/worksheet/state/GoogleSheetsWorksheetStateQueryService.ts
@@ -0,0 +1,37 @@
+﻿import {
+    GoogleSheetsWorksheetStateConfiguration,
+} from 'dressdiscover/api/models/worksheet/configuration/google_sheets_worksheet_state_configuration';
+import { WorksheetStateQueryService } from 'dressdiscover/api/services/worksheet/state/worksheet_state_query_service';
+
+export class GoogleSheetsWorksheetStateQueryService implements WorksheetStateQueryService {
+    constructor(private readonly configuration: GoogleSheetsWorksheetStateConfiguration) {
+    }
+
+    // getWorksheetState(kwds: { id: WorksheetStateId; }): Promise<WorksheetState> {
+    //     return new Promise((resolve, reject) => {
+    //         const jsonString = localStorage.getItem(LocalStorageWorksheetStateQueryService.getWorksheetStateItemKey(kwds.id));
+    //         if (jsonString == null) {
+    //             reject(new NoSuchWorksheetStateException({ id: kwds.id }));
+    //             return;
+    //         }
+    //         resolve(WorksheetState.fromThryftJsonObject(JSON.parse(jsonString)));
+    //     });
+    // }
+
+    // getWorksheetStateIds(): Promise<WorksheetStateId[]> {
+    //     let result: WorksheetStateId[] = [];
+    //     for (var keyI = 0; ; keyI++) {
+    //         const key = localStorage.key(keyI);
+    //         if (key == null) {
+    //             break;
+    //         }
+    //         if (!key.match("^" + LocalStorageWorksheetStateQueryService._WORKSHEET_ITEM_KEY_PREFIX)) {
+    //             continue;
+    //         } else if (key.length == LocalStorageWorksheetStateQueryService._WORKSHEET_ITEM_KEY_PREFIX.length) {
+    //             continue;
+    //         }
+    //         result.push(WorksheetStateId.parse(key.substr(LocalStorageWorksheetStateQueryService._WORKSHEET_ITEM_KEY_PREFIX.length)));
+    //     }
+    //     return new Promise((resolve, reject) => resolve(result));
+    // }
+}
