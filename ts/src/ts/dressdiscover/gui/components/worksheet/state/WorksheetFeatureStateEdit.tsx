60b4da66581fdcbe69ad4229aac642e5d1268c2e
Minor G <minorg@server.fake>
2019-04-23 11:30:50 -0500

    break out component

--- ts/src/ts/dressdiscover/gui/components/worksheet/state/WorksheetFeatureStateEdit.tsx
+++ ts/src/ts/dressdiscover/gui/components/worksheet/state/WorksheetFeatureStateEdit.tsx
@@ -1,20 +1,15 @@
-import * as classnames from 'classnames';
 import { WorksheetFeatureValueId } from 'dressdiscover/api/models/worksheet/worksheet_feature_value_id';
-import { WorksheetDescriptionComponent } from 'dressdiscover/gui/components/worksheet/state/WorksheetDescriptionComponent';
+import { WorksheetFeatureValueStateEdit } from 'dressdiscover/gui/components/worksheet/state/WorksheetFeatureValueStateEdit';
 import { WorksheetStateFrame } from 'dressdiscover/gui/components/worksheet/state/WorksheetStateFrame';
 import { WorksheetStateGetter } from 'dressdiscover/gui/components/worksheet/state/WorksheetStateGetter';
 import { WorksheetStateMarkParser } from 'dressdiscover/gui/components/worksheet/state/WorksheetStateMarkParser';
-import {
-    WorksheetFeatureValueDefinitionWrapper,
-} from 'dressdiscover/gui/models/worksheet/definition/WorksheetFeatureValueDefinitionWrapper';
 import { WorksheetStateWrapper } from 'dressdiscover/gui/models/worksheet/state/WorksheetStateWrapper';
 import { CurrentUserStore } from 'dressdiscover/gui/stores/current_user/CurrentUserStore';
 import { History } from 'history';
 import { inject } from 'mobx-react';
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
-import { Card, CardBody, Collapse, Container, Row } from 'reactstrap';
-import CardHeader from 'reactstrap/lib/CardHeader';
+import { Container, Row } from 'reactstrap';
 
 export class WorksheetFeatureStateEdit extends React.Component<RouteComponentProps<WorksheetStateMarkRouteParams>> {
     render() {
@@ -91,89 +86,5 @@ class WorksheetFeatureStateEditImpl extends React.Component<WorksheetFeatureStat
     save() {
         const { currentUserStore, worksheetState } = this.props;
         currentUserStore!.currentUserServices.worksheetStateCommandService.putWorksheetState({ state: worksheetState.toWorksheetState() });
-    }
-}
-
-interface WorksheetFeatureValueStateEditProps {
-    featureValueDefinition: WorksheetFeatureValueDefinitionWrapper;
-    onToggleSelected: (featureValueId: WorksheetFeatureValueId) => void;
-    selected: boolean;
-}
-
-interface WorksheetFeatureValueStateEditState {
-    descriptionShown: boolean;
-}
-
-class WorksheetFeatureValueStateEdit extends React.Component<WorksheetFeatureValueStateEditProps, WorksheetFeatureValueStateEditState> {
-    constructor(props: WorksheetFeatureValueStateEditProps) {
-        super(props);
-        this.onToggleDescription = this.onToggleDescription.bind(this);
-        this.onToggleSelected = this.onToggleSelected.bind(this);
-        this.state = { descriptionShown: false };
-    }
-
-    onToggleDescription() {
-        this.setState((prevState) => ({ descriptionShown: !prevState.descriptionShown }));
-    }
-
-    onToggleSelected() {
-        this.props.onToggleSelected(this.props.featureValueDefinition.id);
-    }
-
-    render() {
-        const { featureValueDefinition: definition } = this.props;
-
-        let thumbnailImgSrc: string | undefined;
-        if (definition.image && definition.image.thumbnailUrl) {
-            if (definition.image.thumbnailUrl.absolute) {
-                thumbnailImgSrc = definition.image.thumbnailUrl.absolute;
-            } else if (definition.image.thumbnailUrl.relative) {
-                thumbnailImgSrc = "https://dressdiscover.org/img/worksheet/" + definition.image.thumbnailUrl.relative;
-            }
-        }
-        if (!thumbnailImgSrc) {
-            thumbnailImgSrc = 'http://via.placeholder.com/200x200?text=Missing%20image';
-        }
-
-        return (
-            <Card className={classnames({ "border-info": this.props.selected, "mb-4": true, "mr-4": true })} style={{ borderWidth: "8px" }}>
-                <CardHeader>
-                    <a onClick={this.onToggleSelected}>{definition.displayName}</a>
-                </CardHeader>
-                <CardBody style={{ width: "240px" }}>
-                    <figure className="figure">
-                        <a onClick={this.onToggleSelected}>
-                            <img
-                                className="figure-img rounded"
-                                src={thumbnailImgSrc}
-                                style={{ height: "200px", width: "200px" }}
-                            />
-                        </a>
-                        {definition.image ? (
-                            <figcaption className="figure-caption" style={{ fontSize: "xx-small" }}>
-                                Image&nbsp;&copy;
-                                <span>{definition.image.rights.author}</span>
-                                <br />License:&nbsp;
-                                <span >{definition.image.rights.license}</span>
-                            </figcaption>
-                        ) : null}
-                    </figure>
-                    {definition.description ? (
-                        <div className="card-text">
-                            <a onClick={this.onToggleDescription}>Description</a>
-                            <div className="float-right">
-                                <a onClick={this.onToggleDescription} style={{ textDecoration: "none" }}>
-                                    <i className={classnames({ fas: true, "fa-chevron-down": this.state.descriptionShown, "fa-chevron-right": !this.state.descriptionShown })}></i>
-                                </a>
-                            </div>
-                            <br />
-                            <Collapse isOpen={this.state.descriptionShown}>
-                                <WorksheetDescriptionComponent description={definition.description}></WorksheetDescriptionComponent>
-                            </Collapse>
-                        </div>
-                    ) : null}
-                </CardBody>
-            </Card>
-        );
     }
 }
