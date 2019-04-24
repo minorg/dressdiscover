d449be3f9aa67f861b4ff8fb737272d85e93750c
Minor G <minorg@server.fake>
2019-04-24 15:20:00 -0500

    gut global.d.ts

--- ts/types/global.d.ts
+++ ts/types/global.d.ts
@@ -1,11 +1,3 @@
-/** Global definitions for development **/
-
-// for style loader
-// declare module '*.css' {
-//   const styles: any;
-//   export = styles;
-// }
-
 declare module 'react-clipboard.js';
 declare module 'react-hammerjs';
 
@@ -13,7 +5,3 @@ interface Exception {
     toJsonObject(): any;
     toString(): string;
 }
-
-// Omit type https://github.com/Microsoft/TypeScript/issues/12215#issuecomment-377567046
-type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
-type PartialPick<T, K extends keyof T> = Partial<T> & Pick<T, K>;
