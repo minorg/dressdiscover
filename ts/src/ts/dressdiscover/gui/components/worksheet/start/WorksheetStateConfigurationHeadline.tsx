1b6b494a6fba4c005d74d50da69b11cd656edeed
Minor G <minorg@server.fake>
2019-05-05 15:08:48 -0500

    change configuration headline verbiage

--- ts/src/ts/dressdiscover/gui/components/worksheet/start/WorksheetStateConfigurationHeadline.tsx
+++ ts/src/ts/dressdiscover/gui/components/worksheet/start/WorksheetStateConfigurationHeadline.tsx
@@ -5,22 +5,28 @@ import * as React from 'react';
 import { Link } from 'react-router-dom';
 
 @inject("currentUserStore")
-export class WorksheetStateConfigurationHeadline extends React.Component<{currentUserStore?: CurrentUserStore}> {
+export class WorksheetStateConfigurationHeadline extends React.Component<{ currentUserStore?: CurrentUserStore }> {
     render() {
         const { currentUserStore } = this.props;
 
         const currentUser = currentUserStore!.currentUser;
         const worksheetStateConfiguration = currentUserStore!.currentUserServices.configuration.state;
 
+        const localStorageText = "storage on this browser / machine";
+
+        if (!currentUser) {
+            return <React.Fragment>{localStorageText} (<Link to={Hrefs.login}>Login</Link> to change to a Google Sheet)</React.Fragment>;
+        }
+
         let text: string;
         if (worksheetStateConfiguration.googleSheets) {
             text = "Google Sheet";
         } else if (worksheetStateConfiguration.localStorage) {
-            text = "storage on this browser / machine";
+            text = localStorageText;
         } else {
             throw new EvalError();
         }
 
-        return <React.Fragment>{text}{currentUser ? <React.Fragment> (<Link to={Hrefs.userSettings}>Change</Link>)</React.Fragment> : null}</React.Fragment>;
+        return <React.Fragment>{text} (<Link to={Hrefs.userSettings}>Change</Link>)</React.Fragment>;
     }
 }
