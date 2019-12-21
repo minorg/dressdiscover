7b6e5eae3af7be22f4391027b6f41244d3d5fcc1
Minor G <minorg@server.fake>
2019-12-21 16:44:26 -0500

    OWL Time-based DateTimeDescription

--- cms/etl/dressdiscover/cms/etl/lib/model/_model.py
+++ cms/etl/dressdiscover/cms/etl/lib/model/_model.py
@@ -1,5 +1,6 @@
 import logging
 from abc import ABC
+from typing import Optional
 
 from rdflib import Graph, Literal, RDF, URIRef
 from rdflib.resource import Resource
@@ -8,12 +9,36 @@ from dressdiscover.cms.etl.lib.namespace import CMS
 
 
 class _Model(ABC):
-    def __init__(self, *, graph: Graph, uri: URIRef):
+    def __init__(self, *, graph: Graph, uri: URIRef, rdf_type: Optional[URIRef] = None):
         assert isinstance(uri, URIRef)
         self._logger = logging.getLogger(self.__class__.__name__)
         self.__resource = graph.resource(uri)
-        self.__resource.add(RDF.type, getattr(CMS, self.__class__.__name__))
+        if rdf_type is None:
+            rdf_type = getattr(CMS, self.__class__.__name__)
+        self.__resource.add(RDF.type, rdf_type)
         self.__uri = uri
+
+    def _get_single_literal(self, predicate) -> Optional[Literal]:
+        """
+        Get a single literal object for the given predicate, or None if absent.
+        This model's resource must only have a single statement with that predicat
+         and the statement's object must be a Literal.
+        """
+        for object_ in self.resource.objects(predicate):
+            if not isinstance(object_, Literal):
+                self._logger.warning("%s has non-Literal object_", predicate)
+                continue
+            return object_
+        return None
+
+    def _get_single_typed_literal(self, predicate, literal_datatype) -> Optional[Literal]:
+        literal = self._get_single_literal(predicate)
+        if literal is None:
+            return None
+        if literal.datatype != literal_datatype:
+            raise ValueError("%s literal has wrong datatype: expected=%s, actual=%s" % (
+                predicate, literal_datatype, literal.datatype))
+        return literal
 
     def _get_single_value(self, predicate, value_type):
         """
@@ -21,16 +46,14 @@ class _Model(ABC):
         This model's resource must only have a single statement with that predicate
         and the statement's object must be a Literal.
         """
-        for object_ in self.resource.objects(predicate):
-            if not isinstance(object_, Literal):
-                self._logger.warning("%s has non-Literal object_", predicate)
-                continue
-            value = object_.toPython()
-            if not isinstance(value, value_type):
-                raise ValueError("%s has unexpected type: expected=%s, actual=%s" % (
-                    predicate, value_type, value.__class__.__name__))
-            return value
-        return None
+        literal = self._get_single_literal(predicate)
+        if literal is None:
+            return None
+        value = literal.toPython()
+        if not isinstance(value, value_type):
+            raise ValueError("%s has unexpected type: expected=%s, actual=%s" % (
+                predicate, value_type, value.__class__.__name__))
+        return value
 
     @property
     def resource(self) -> Resource:
--- cms/etl/dressdiscover/cms/etl/lib/namespace.py
+++ cms/etl/dressdiscover/cms/etl/lib/namespace.py
@@ -3,3 +3,4 @@ from rdflib import Namespace
 EXIF = Namespace("http://www.w3.org/2003/12/exif/ns#")
 CMS = Namespace("http://www.dressdiscover.org/ns/cms#")
 PROV = Namespace("http://www.w3.org/ns/prov#")
+TIME = Namespace("http://www.w3.org/2006/time#")
