c6d423db43774eb6f94a389fa492d1917c898e59
Minor G <minorg@server.fake>
2020-05-09 13:40:42 -0400

    fix path

--- cms/etl/dressdiscover/cms/etl/path.py
+++ cms/etl/dressdiscover/cms/etl/path.py
@@ -1,4 +1,4 @@
 from pathlib import Path
 
-DATA_DIR_PATH = Path(__file__).parent.parent.parent.parent.parent.absolute() / "data"
+DATA_DIR_PATH = Path(__file__).parent.parent.parent.parent.absolute() / "data"
 assert DATA_DIR_PATH.is_dir(), DATA_DIR_PATH
