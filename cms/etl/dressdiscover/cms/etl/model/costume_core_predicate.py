af9d66e1c78dc1a99be0bfcf5a11af316ac1e13c
Minor G <minorg@server.fake>
2020-06-01 17:23:37 -0400

    use typing.NamedTuple

--- cms/etl/dressdiscover/cms/etl/model/costume_core_predicate.py
+++ cms/etl/dressdiscover/cms/etl/model/costume_core_predicate.py
@@ -1,20 +1,7 @@
-class CostumeCorePredicate:
-    def __init__(self, *, display_name_en: str, id: str, uri: str):
-        self.__display_name_en = display_name_en
-        self.__id = id
-        self.__uri = uri
+from typing import NamedTuple
 
-    @property
-    def id(self) -> str:
-        return self.__id
 
-    @property
-    def display_name_en(self) -> str:
-        return self.__display_name_en
-
-    def __repr__(self):
-        return f"{self.__class__.__name__}(display_name_en='''{self.display_name_en}''', id='{self.id}', uri='{self.uri}')"
-
-    @property
-    def uri(self) -> str:
-        return self.__uri
+class CostumeCorePredicate(NamedTuple):
+    display_name_en: str
+    id: str
+    uri: str
