05d5de2bf7930ca3704d75bb891da476a2a8174a
Minor G <minorg@server.fake>
2020-06-03 19:05:55 -0400

    add more ontology metadata

--- cms/etl/dressdiscover/cms/etl/pipeline/costume_core/costume_core_transformer.py
+++ cms/etl/dressdiscover/cms/etl/pipeline/costume_core/costume_core_transformer.py
@@ -15,8 +15,26 @@ from dressdiscover.cms.etl.namespace import CC
 
 
 class CostumeCoreTransformer(_Transformer):
+    def __init__(self, *, ontology_version: str):
+        _Transformer.__init__(self)
+        self.__ontology_version = ontology_version
+
+    def __add_ontology(self, graph: Graph):
+        ontology_resource = graph.resource(URIRef(str(CC)[:-1]))
+        ontology_resource.add(RDF.type, OWL.Ontology)
+        ontology_resource.add(OWL.versionIRI, CC[self.__ontology_version])
+        ontology_resource.add(OWL.versionInfo, Literal(self.__ontology_version))
+        ontology_resource.add(DCTERMS.title, Literal("Costume Core Ontology"))
+        ontology_resource.add(DCTERMS.creator, Literal("Arden Kirkland"))
+        ontology_resource.add(DCTERMS.contributor, Literal("Minor Gordon"))
+        ontology_resource.add(DCTERMS.rights, Literal(
+            "This ontology is distributed under a Creative Commons BY SA 4.0 license  - https://creativecommons.org/licenses/by-sa/4.0/"))
+        ontology_resource.add(RDFS.comment, Literal(
+            "The Costume Core ontology is for describing artifacts of historic clothing, and is meant to build upon VRA Core and Dublin Core. Work to develop this ontology was part of the Costume Core Toolkit project, funded by a Visual Resources Association Foundation Project Grant in 2019-2020. More information is at http://ardenkirkland.com/costumecore"))
+
     def __parse_predicates(self, cc_predicates_csv_file_path: Path) -> Tuple[CostumeCorePredicate, ...]:
         predicates = []
+
         with open(cc_predicates_csv_file_path, "r") as cc_predicates_csv_file:
             for row in csv.DictReader(cc_predicates_csv_file):
                 predicates.append(
@@ -80,14 +98,11 @@ class CostumeCoreTransformer(_Transformer):
         graph.namespace_manager.bind("cc", CC)
         graph.namespace_manager.bind("owl", OWL)
 
-        cc_ontology_uri = URIRef(str(CC)[:-1])
-        graph.add((cc_ontology_uri, RDF.type, OWL.Ontology))
-        graph.add((cc_ontology_uri, OWL.versionIRI, CC["0.4.0"]))
-
         # Annotation properties
         for annotation_property_local in ("creator", "description", "identifier", "license", "rights", "source"):
             graph.add((DCTERMS[annotation_property_local], RDF.type, OWL.AnnotationProperty))
 
+        self.__add_ontology(graph=graph)
         self.__transform_predicates(graph=graph, predicates=predicates, terms=terms)
         self.__transform_terms(graph=graph, terms=terms)
 
