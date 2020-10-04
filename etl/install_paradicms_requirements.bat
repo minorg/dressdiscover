867ef97a66a43800554762c910190abe0d79e71e
Minor G <minorg@server.fake>
2020-10-04 16:17:57 -0400

    fix install_paradicms_requirements.bat path

--- etl/install_paradicms_requirements.bat
+++ etl/install_paradicms_requirements.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 cd %~dp0
 CALL venv\Scripts\activate.bat
-pip install -r ..\..\..\paradicms\etl\requirements.txt
-pip install -r ..\..\..\pastpy\requirements.txt
+pip install -r ..\..\paradicms\etl\requirements.txt
+pip install -r ..\..\pastpy\requirements.txt
