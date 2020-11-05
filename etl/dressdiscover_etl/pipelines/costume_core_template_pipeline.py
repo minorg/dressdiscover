a978dcf9d6b9704893f603dfaa534ef91a75ad6d
Minor G <minorg@server.fake>
2020-11-05 09:03:06 -0500

    CostumeCoreTemplatePipeline: make own site on AWS

--- etl/dressdiscover_etl/pipelines/costume_core_template_pipeline.py
+++ etl/dressdiscover_etl/pipelines/costume_core_template_pipeline.py
@@ -1,9 +1,13 @@
+from pathlib import Path
 from typing import Optional
 
 from paradicms_etl._loader import _Loader
 from paradicms_etl._pipeline import _Pipeline
+from paradicms_etl.image_archivers.s3_image_archiver import S3ImageArchiver
 from paradicms_etl.loaders.composite_loader import CompositeLoader
 from paradicms_etl.loaders.default_loader import DefaultLoader
+from paradicms_etl.loaders.gui.gui_loader import GuiLoader
+from paradicms_etl.loaders.gui.s3_gui_deployer import S3GuiDeployer
 
 from dressdiscover_etl.extractors.costume_core_airtable_extractor import (
     CostumeCoreAirtableExtractor,
@@ -16,9 +20,9 @@ from dressdiscover_etl.transformers.costume_core_airtable_transformer import (
 )
 
 
-class CostumeCoreTemplateAirtablePipeline(_Pipeline):
+class CostumeCoreTemplatePipeline(_Pipeline):
     __BASE_ID = "appgU92SdGTwPIVNg"
-    __ID = "costume-core-template-airtable"
+    __ID = "costume_core_template"
 
     def __init__(self, api_key: str, loader: Optional[_Loader] = None, **kwds):
         if loader is None:
@@ -28,9 +32,21 @@ class CostumeCoreTemplateAirtablePipeline(_Pipeline):
                         pipeline_id=self.__ID, **kwds
                     ),
                     DefaultLoader(pipeline_id=self.__ID, **kwds),
+                    GuiLoader(
+                        gui="bootstrap-collection",
+                        deployer=S3GuiDeployer(
+                            s3_bucket_name="costumecoretemplate.dressdiscover.org",
+                            **kwds,
+                        ),
+                        image_archiver=S3ImageArchiver(
+                            s3_bucket_name="dressdiscover-images", **kwds
+                        ),
+                        pipeline_id=self.__ID,
+                        **kwds,
+                    ),
                 ),
                 pipeline_id=self.__ID,
-                **kwds
+                **kwds,
             )
 
         _Pipeline.__init__(
@@ -48,16 +64,17 @@ class CostumeCoreTemplateAirtablePipeline(_Pipeline):
                 institution_uri="http://www.ardenkirkland.com/costumecore/",
                 institution_rights="Copyright Arden Kirkland. All rights reserved.",
                 pipeline_id=self.__ID,
-                **kwds
+                **kwds,
             ),
-            **kwds
+            **kwds,
         )
 
     @classmethod
     def add_arguments(cls, arg_parser):
         _Pipeline.add_arguments(arg_parser)
+        _Pipeline._add_aws_credentials_arguments(arg_parser)
         arg_parser.add_argument("--api-key", required=True)
 
 
 if __name__ == "__main__":
-    CostumeCoreTemplateAirtablePipeline.main()
+    CostumeCoreTemplatePipeline.main()
